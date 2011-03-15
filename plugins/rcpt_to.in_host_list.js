719148dd116e1bb793eea15c2fbc2ac4f2f0c893
Matt Sergeant <helpme+github@gmail.com>
2011-03-14 20:50:57 -0400

    Check rcpt_to is in a list of hosts

---
+++ plugins/rcpt_to.in_host_list.js
@@ -0,0 +1,42 @@
+// Check RCPT TO domain is in host list
+
+var smtp = require('../constants');
+
+exports.hook_rcpt = function(callback, connection, params) {
+    var rcpt = params[0];
+    // Check for RCPT TO without an @ first - ignore those here
+    if (!rcpt.match(/@/)) {
+        return callback(smtp.cont);
+    }
+    
+    this.loginfo("Checking if " + rcpt + " host is in host_list");
+    
+    var matches = rcpt.match(/@([^@>]*)>?/);
+    if (!matches) {
+        this.logerror("TO address does not parse: " + rcpt);
+        return callback(smtp.deny, "TO address does not parse");
+    }
+    
+    var domain = matches[1];
+    var host_list = this.config.get('host_list', 'list');
+    var allow_subdomain = this.config.get('host_list.ini', 'ini').main.allow_subdomains;
+    
+    var i;
+    for (i in host_list) {
+        this.logdebug("checking " + domain + " against " + host_list[i]);
+        var tmp_domain = domain;
+        while (tmp_domain.match('.')) {
+            if (host_list[i] === tmp_domain) {
+                return callback(smtp.ok);
+            }
+            if (allow_subdomain) {
+                tmp_domain = tmp_domain.replace(/^[^\.]*\./, '');
+            }
+            else {
+                break;
+            }
+        }
+    }
+    
+    callback(smtp.cont);
+}
