896330bc3aaedf16d105314a6ce85c982ecd7772
Matt Sergeant <helpme+github@gmail.com>
2011-04-02 19:00:10 -0400

    Fixed deny hooks again

--- plugins.js
+++ plugins.js
@@ -11,43 +11,39 @@ var utils       = require('./utils');
 var plugin_path = process.env.HARAKA ? path.join(process.env.HARAKA, 'plugins') : './plugins';
 // These are the hooks that qpsmtpd implements - I should get around
 // to supporting them all some day... :-/
-var hooks = [
-    'connect',
-    'pre-connection',
-    'connect',
-    'ehlo_parse',
-    'ehlo',
-    'helo_parse',
-    'helo',
-    'auth_parse',
-    'auth',
-    'auth-plain',
-    'auth-login',
-    'auth-cram-md5',
-    'rcpt_parse',
-    'rcpt_pre',
-    'rcpt',
-    'mail_parse',
-    'mail',
-    'mail_pre', 
-    'data',
-    'data_headers_end',
-    'data_post',
-    'queue_pre',
-    'queue',
-    'queue_post',
-    'vrfy',
-    'noop',
-    'quit',
-    'reset_transaction',
-    'disconnect',
-    'post-connection',
-    'unrecognized_command',
-    'deny',
-    'ok',
-    'received_line',
-    'help'
-];
+var regular_hooks = {
+    'connect':1,
+    'pre-connection': 1,
+    'connect': 1,
+    'ehlo_parse': 1,
+    'ehlo': 1,
+    'helo_parse': 1,
+    'helo': 1,
+    'auth_parse': 1,
+    'auth': 1,
+    'auth-plain': 1,
+    'auth-login': 1,
+    'auth-cram-md5': 1,
+    'rcpt_parse': 1,
+    'rcpt_pre': 1,
+    'rcpt': 1,
+    'mail_parse': 1,
+    'mail': 1,
+    'mail_pre': 1, 
+    'data': 1,
+    'data_headers_end': 1,
+    'data_post': 1,
+    'queue_pre': 1,
+    'queue': 1,
+    'queue_post': 1,
+    'vrfy': 1,
+    'noop': 1,
+    'quit': 1,
+    'reset_transaction': 1,
+    'disconnect': 1,
+    'unrecognized_command': 1,
+    'help': 1
+};
 
 function Plugin(name) {
     this.name = name;
@@ -124,7 +120,7 @@ plugins.load_plugins();
 plugins.run_hooks = function (hook, connection, params) {
     logger.logdebug("running " + hook + " hooks");
     
-    if (connection.hooks_to_run.length) {
+    if (regular_hooks[hook] && connection.hooks_to_run.length) {
         throw new Error("We are already running hooks! Fatal error!");
     }
     
@@ -164,7 +160,7 @@ plugins.run_next_hook = function(hook, connection) {
         {
             var respond_method = hook + "_respond";
             if (item && utils.in_array(retval, [constants.deny, constants.denysoft, constants.denydisconnect])) {
-                connection.deny_respond = function (retval, msg) {
+                connection.deny_respond = function () {
                     connection[respond_method](retval, msg);
                 };
                 plugins.run_hooks('deny', connection, [retval, msg, item[0].name, item[1], item[2]]);
