c5cb4e62613b55038241c6fce1808893115882d8
Matt Sergeant <helpme+github@gmail.com>
2011-04-05 12:24:06 -0400

    Up version to 0.2

--- haraka.js
+++ haraka.js
@@ -3,7 +3,7 @@
 var logger = require('./logger');
 var server = require('./server');
 
-exports.version = '0.1';
+exports.version = '0.2';
 
 process.on('uncaughtException', function (err) {
     if (err.stack) {
