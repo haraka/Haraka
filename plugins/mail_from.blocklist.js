802742604ff8696c78baee0ce489ad01a657049f
Matt Sergeant <helpme+github@gmail.com>
2011-04-11 17:47:38 -0400

    Added a mail from blocklist

---
+++ plugins/mail_from.blocklist.js
@@ -0,0 +1,11 @@
+// Block mail from matching anything in the list
+var utils = require('./utils');
+
+exports.hook_mail = function (next, connection, params) {
+    var mail_from = params[0];
+    var list = this.config.get('mail_from.blocklist', 'list');
+    if (utils.in_array(mail_from, list)) {
+        return next(DENY, "Mail from you is not allowed here");
+    }
+    return next();
+}
