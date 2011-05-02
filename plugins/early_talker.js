1fc6bfd2a4ba055fea2297f57f8f29aa744b7833
Matt Sergeant <helpme+github@gmail.com>
2011-05-01 23:05:02 -0400

    Load pause value in the hook, so it is auto-reloaded

--- plugins/early_talker.js
+++ plugins/early_talker.js
@@ -1,13 +1,13 @@
 // This plugin checks for clients that talk before we sent a response
 
 exports.register = function() {
-    this.pause = this.config.get('early_talker.pause', 'value');
     this.register_hook('data', 'check_early_talker');
 };
 
 exports.check_early_talker = function(next, connection) {
-    if (this.pause) {
-        setTimeout(function () { _check_early_talker(connection, next) }, this.pause);
+    var pause = this.config.get('early_talker.pause');
+    if (pause) {
+        setTimeout(function () { _check_early_talker(connection, next) }, pause);
     }
     else {
         _check_early_talker(connection, next);
