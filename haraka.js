666a469d613154bc80a8cb9693b93c060c407e00
arlolra <arlolra@gmail.com>
2011-08-01 14:22:37 -0700

    prepare for v0.5

--- haraka.js
+++ haraka.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
-var path = require('path');
-process.env.HARAKA = process.env.HARAKA || path.resolve('.');
-require.paths.unshift(path.join(process.env.HARAKA, 'node_modules'));
-
+var path   = require('path');
 var fs     = require('fs');
 var logger = require('./logger');
 var server = require('./server');
+
+process.env.HARAKA = process.env.HARAKA || path.resolve('.');
+try {
+  require.paths.unshift(path.join(process.env.HARAKA, 'node_modules'));
+} catch(e) {
+  logger.loginfo('No require paths. Must be node v0.5');
+  process.env.NODE_PATH += ':' + process.env.HARAKA + '/node_modules';
+}
 
 exports.version = JSON.parse(
         fs.readFileSync(path.join(__dirname, './package.json'), 'utf8')
