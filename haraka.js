3239aee04a020512717c6470f2c480554d06eefe
Matt Sergeant <helpme+github@gmail.com>
2011-08-02 11:47:06 -0400

    Fixed some issues with changes to support node v0.5+

--- haraka.js
+++ haraka.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
 var path   = require('path');
+
+process.env.HARAKA = process.env.HARAKA || path.resolve('.');
+
 var fs     = require('fs');
 var logger = require('./logger');
 var server = require('./server');
 
-process.env.HARAKA = process.env.HARAKA || path.resolve('.');
 try {
   require.paths.unshift(path.join(process.env.HARAKA, 'node_modules'));
 } catch(e) {
   logger.loginfo('No require paths. Must be node v0.5');
-  process.env.NODE_PATH += ':' + process.env.HARAKA + '/node_modules';
+  process.env.NODE_PATH += ':' + path.join(process.env.HARAKA, 'node_modules');
 }
 
 exports.version = JSON.parse(
