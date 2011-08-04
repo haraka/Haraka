afb5bd216c4f16e7a48858141776ed8b20b2a7fd
Matt Sergeant <helpme+github@gmail.com>
2011-08-04 14:41:15 -0400

    Fixed running under 0.5 again

--- haraka.js
+++ haraka.js
@@ -2,18 +2,18 @@
 
 var path   = require('path');
 
+// this must be set before "server.js" is loaded
 process.env.HARAKA = process.env.HARAKA || path.resolve('.');
-
-var fs     = require('fs');
-var logger = require('./logger');
-var server = require('./server');
-
 try {
   require.paths.unshift(path.join(process.env.HARAKA, 'node_modules'));
 } catch(e) {
   logger.loginfo('No require paths. Must be node v0.5');
   process.env.NODE_PATH += ':' + path.join(process.env.HARAKA, 'node_modules');
 }
+
+var fs     = require('fs');
+var logger = require('./logger');
+var server = require('./server');
 
 exports.version = JSON.parse(
         fs.readFileSync(path.join(__dirname, './package.json'), 'utf8')
