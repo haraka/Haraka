8e9a3038781ce79ee1fb15613561664dd409b183
Matt Sergeant <helpme+github@gmail.com>
2011-09-01 15:15:36 -0400

    Added TLS support

---
+++ plugins/tls.js
@@ -0,0 +1,30 @@
+// Enables TLS. This is built into the server anyway, but enabling this plugin
+// just advertises it.
+
+var utils = require('./utils');
+
+// To create a key:
+// openssl req -x509 -nodes -days 2190 -newkey rsa:1024 \
+//         -keyout config/tls_key.pem -out config/tls_cert.pem
+
+exports.hook_capabilities = function (next, connection) {
+    connection.capabilities.push('STARTTLS');
+    connection.notes.tls_enabled = 1;
+    next();
+}
+
+exports.hook_unrecognized_command = function (next, connection, params) {
+    var key  = this.config.get('tls_key.pem',  'list').join("\n");
+    var cert = this.config.get('tls_cert.pem', 'list').join("\n");
+    var options = { key: key, cert: cert };
+
+    /* Respond to STARTTLS command. */
+    connection.respond(220,"Go ahead.");
+    /* Upgrade the connection to TLS. */
+    connection.client.upgrade(options); // Use the options which were saved by starttls.createServer().
+    /* Force the startup protocol to repeat. */
+    connection.uuid = utils.uuid();
+    connection.reset_transaction();
+    connection.hello_host = undefined;
+    connection.using_tls = true;
+}
