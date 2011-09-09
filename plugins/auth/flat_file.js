86e854fbaa6bba38021afbd3d4efbb3bec910139
schamane <schamane@myeburg.net>
2011-09-09 12:25:08 +0200

    refactore flat_file plugin for easier to add new auth methods

--- plugins/auth/flat_file.js
+++ plugins/auth/flat_file.js
@@ -1,47 +1,32 @@
 // Auth against a flat file
 
-var crypto = require('crypto');
+var crypto = require('crypto'),
+    AUTH_COMMAND = 'AUTH',
+    AUTH_METHOD_CRAM_MD5 = 'CRAM-MD5',
+    AUTH_METHOD_LOGIN = 'LOGIN',
+    LOGIN_STRING1 = 'VXNlcm5hbWU6', //UserLogin: base64 coded
+    LOGIN_STRING2 = 'UGFzc3dvcmQ6'; //Password: base64 coded
 
 exports.hook_capabilities = function (next, connection) {
     var config = this.config.get('auth_flat_file.ini', 'ini'),
-	methods = config.methods['allowed'].split(',');
-    if(methods.length > 0)
-	connection.capabilities.push('AUTH ' + methods.join(' '));
+	methods = (config.methods && config.methods.allowed ) ? config.methods.allowed.split(',') : null;
+    if(methods.length > 0) {
+	    connection.capabilities.push('AUTH ' + methods.join(' '));
+        connection.notes.allowed_auth_methods = methods;
+    }
     next();
-}
+};
 
 exports.hook_unrecognized_command = function (next, connection, params) {
-    if (connection.notes.auth_flat_file_ticket) {
-        var credentials = unbase64(params[0]).split(' ');
-        return this.check_user(next, connection, credentials, 'MD5');
-    }else if (connection.notes.auth_login_flat_file_ticket && !connection.notes.auth_login_flat_file_login) {
-        var login = unbase64(params[0]);
-        connection.respond(334, 'UGFzc3dvcmQ6');
-        connection.notes.auth_login_flat_file_login = login;
-        return next(OK);
-    }else if (connection.notes.auth_login_flat_file_ticket && connection.notes.auth_login_flat_file_login) {
-        var pass = unbase64(params[0]),
-    	    credentials = [
-		connection.notes.auth_login_flat_file_login,
-		pass
-	    ];
-        return this.check_user(next, connection, [connection.notes.auth_login_flat_file_login, pass], 'PLAIN');
-    }
-    else if (params[0] === 'AUTH' && params[1] === 'CRAM-MD5') {
-        var ticket = '<' + hexi(Math.floor(Math.random() * 1000000)) + '.' +
-                    hexi(Date.now()) + '@' + this.config.get('me') + '>';
-        this.loginfo("ticket: " + ticket);
-        connection.respond(334, base64(ticket));
-        connection.notes.auth_flat_file_ticket = ticket;
-        return next(OK);
-    }
-    else if (params[0] === 'AUTH' && params[1] === 'LOGIN') {
-        connection.respond(334, 'VXNlcm5hbWU6');
-        connection.notes.auth_login_flat_file_ticket = 'UserName:';
-        return next(OK);
+    if(params[0] === AUTH_COMMAND && params[1]) {
+        return this.select_auth_method(next, connection, params[1]);
+    } else if (connection.notes.auth_method === AUTH_METHOD_CRAM_MD5 && connection.notes.auth_flat_file_ticket) {
+        return this.auth_cram_md5 (next, connection, params);
+    } else if (connection.notes.auth_method === AUTH_METHOD_LOGIN) {
+        return this.auth_login(next, connection, params);
     }
     return next();
-}
+};
 
 exports.check_user = function (next, connection, credentials, method) {
     if (!(credentials[0] && credentials[1])) {
@@ -60,10 +45,10 @@ exports.check_user = function (next, connection, credentials, method) {
     var clear_pw = config.users[credentials[0]],
 	hmac_pw = clear_pw;
 
-    if(method === "MD5") {
-	var hmac = crypto.createHmac('md5', clear_pw);
-	hmac.update(connection.notes.auth_flat_file_ticket);
-	hmac_pw = hmac.digest('hex');
+    if(method === AUTH_METHOD_CRAM_MD5) {
+	    var hmac = crypto.createHmac('md5', clear_pw);
+	    hmac.update(connection.notes.auth_flat_file_ticket);
+	    hmac_pw = hmac.digest('hex');
     }
     
     this.loginfo("comparing " + hmac_pw + ' to ' + credentials[1]);
@@ -77,7 +62,52 @@ exports.check_user = function (next, connection, credentials, method) {
         connection.reset_transaction();
     }
     return next(OK);
-}
+};
+
+exports.select_auth_method = function(next, connection, method) {
+    if(connection.notes.allowed_auth_methods.indexOf(method) !== -1) {
+        connection.notes.auth_method = method;
+        if(method === AUTH_METHOD_LOGIN) {
+            return this.auth_login(next, connection);
+        } else if( method === AUTH_METHOD_CRAM_MD5) {
+            return this.auth_cram_md5(next, connection);
+        }
+    }
+    return next();
+};
+
+exports.auth_login = function(next, connection, params) {
+    if (connection.notes.auth_login_asked_login && !connection.notes.auth_login_userlogin) {
+        var login = unbase64(params[0]);
+        connection.respond(334, LOGIN_STRING2);
+        connection.notes.auth_login_userlogin = login;
+        return next(OK);
+    }else if (connection.notes.auth_login_userlogin) {
+        var credentials = [
+		        connection.notes.auth_login_userlogin,
+		        unbase64(params[0])
+	        ];
+        return this.check_user(next, connection, credentials, AUTH_METHOD_LOGIN);
+    }
+    
+    connection.respond(334, LOGIN_STRING1);
+    connection.notes.auth_login_asked_login = true;
+    return next(OK);
+};
+
+exports.auth_cram_md5 = function(next, connection, params) {
+    if(params) {
+        var credentials = unbase64(params[0]).split(' ');
+        return this.check_user(next, connection, credentials, AUTH_METHOD_CRAM_MD5);
+    }
+    
+    var ticket = '<' + hexi(Math.floor(Math.random() * 1000000)) + '.' +
+                    hexi(Date.now()) + '@' + this.config.get('me') + '>';
+    this.loginfo("ticket: " + ticket);
+    connection.respond(334, base64(ticket));
+    connection.notes.auth_flat_file_ticket = ticket;
+    return next(OK);
+};
 
 function hexi (number) {
     return String(Math.abs(parseInt(number)).toString(16));
