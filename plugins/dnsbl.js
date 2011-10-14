7898f6f214852a712b257a4f6c34698e4646a450
ddub <git@dub.to>
2011-10-14 08:28:04 -0400

    IPv6 Blacklists

--- plugins/dnsbl.js
+++ plugins/dnsbl.js
@@ -1,9 +1,11 @@
 // dnsbl plugin
 
 var dns   = require('dns');
+var utils = require('./utils.js');
 
 exports.register = function() {
     this.zones = this.config.get('dnsbl.zones', 'list');
+    this.v6zones = this.config.get('dnsbl6.zones', 'list');
     this.register_hook('connect', 'check_ip');
 }
 
@@ -11,17 +13,37 @@ exports.check_ip = function(next, connection) {
     this.logdebug("check_ip: " + connection.remote_ip);
     
     var ip = new String(connection.remote_ip);
-    var reverse_ip = ip.split('.').reverse().join('.');
-    
-    if (!this.zones || !this.zones.length) {
-        this.logerror("No zones");
+    var reverse_ip, zones;
+    if (utils.is_ipv4(ip)) {
+        reverse_ip = ip.split('.').reverse().join('.');
+
+        if (!this.zones || !this.zones.length) {
+            this.logerror("No zones");
+            return next();
+        }
+
+        zones = this.zones
+    }
+    else if (utils.is_ipv6(ip)) {
+        reverse_ip = utils.expand_ipv6(ip).replace(/:/g,'').split('').reverse().join('.');
+
+        if (!this.v6zones || !this.v6zones.length) {
+            this.logerror("No ipv6 zones");
+            return next();
+        }
+
+        zones = this.v6zones
+    }
+    else {
+        this.logerror(ip + " is not a valid internet address!");
         return next();
     }
+    
     
     var remaining_zones = [];
     
     var self = this;
-    this.zones.forEach(function(zone) {
+    zones.forEach(function(zone) {
         self.logdebug("Querying: " + reverse_ip + "." + zone);
         dns.resolve(reverse_ip + "." + zone, "TXT", function (err, value) {
             if (!remaining_zones.length) return;
--- utils.js
+++ utils.js
@@ -59,3 +59,96 @@ exports.ISODate = function (d) {
       + pad(d.getUTCMinutes())+':'
       + pad(d.getUTCSeconds())+'Z'
 }
+
+// All scanadlously taken from http://ipv6blog.net/ipv6-validation-javascript/
+// substr_count
+//
+// Support function; a javascript version of an original PHP function
+// Found at: http://kevin.vanzonneveld.net
+
+function substr_count (haystack, needle, offset, length) {
+    var pos = 0, cnt = 0;
+
+    haystack += '';
+    needle += '';
+    if (isNaN(offset)) {offset = 0;}
+    if (isNaN(length)) {length = 0;}
+    offset--;
+
+    while ((offset = haystack.indexOf(needle, offset+1)) != -1) {
+        if (length > 0 && (offset+needle.length) > length) {
+            return false;
+        }
+        else {
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+// is_ipv4
+//
+// Test for a valid dotted IPv4 address
+//
+// Ported from: http://www.dijksterhuis.org/regular-expressions-csharp-practical-use/
+var is_ipv4 = exports.is_ipv4 = function (ip) {
+   var match = ip.match(/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/);
+   return match != null;
+}
+
+// is_ipv6
+//
+// Test if the input is a valid ipv6 address. Javascript version of an original PHP function.
+//
+// Ported from: http://crisp.tweakblogs.net/blog/2031
+
+exports.is_ipv6 = function (ip) {
+    // Test for empty address
+    if (ip.length<3) {
+        return ip === "::";
+    }
+
+    // Check if part is in IPv4 format
+    if (ip.indexOf('.')>0) {
+        lastcolon = ip.lastIndexOf(':');
+
+        if (!(lastcolon && is_ipv4(ip.substr(lastcolon + 1))))
+            return false;
+
+        // replace IPv4 part with dummy
+        ip = ip.substr(0, lastcolon) + ':0:0';
+    }
+
+    // Check uncompressed
+    if (ip.indexOf('::')<0) {
+        var match = ip.match(/^(?:[a-f0-9]{1,4}:){7}[a-f0-9]{1,4}$/i);
+        return match !== null;
+    }
+
+    // Check colon-count for compressed format
+    if (substr_count(ip, ':') < 8) {
+        var match = ip.match(/^(?::|(?:[a-f0-9]{1,4}:)+):(?:(?:[a-f0-9]{1,4}:)*[a-f0-9]{1,4})?$/i);
+        return match !== null;
+    }
+
+    // Not a valid IPv6 address
+    return false;
+}
+
+exports.expand_ipv6 = function (ip) {
+    function pad(n) {return (new Array(5-n.length).join('0'))+n}
+    if (ip.indexOf('::')>=0) {
+        // Sigh, magic number time, we want 7 colons in the final string
+        // We count the number of colons that are already there to decide how many to add (7 - count)
+        // We then reduce the count by two (7 - (count - 2)) because of the double colon
+        // The are prefixing the replacement with a colon so remove one (7 - (count - 2) - 1)
+        // and then we add one for the size of the array (7 - (count - 2) - 1 + 1)
+        ip = ip.replace('::', ':' + new Array(9 - substr_count(ip, ':')).join('0:'));
+    }
+    var gobbles = ip.split(':');
+    for (var gobble=0; gobble < 8; gobble++) {
+        gobbles[gobble]=pad(gobbles[gobble]);
+    }
+    return gobbles.join(':');
+}
