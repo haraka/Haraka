1dc7ddc6692c90ce50efef7429cc9a4af24b4505
Christopher Mooney <chris@dod.net>
2012-02-07 11:27:18 -0800

    More advanced MAIL FROM blocking.

--- docs/plugins/mail_from.blocklist.md
+++ docs/plugins/mail_from.blocklist.md
@@ -1,7 +1,11 @@
 mail_from.blocklist
 ===================
 
-This mail blocks MAIL_FROM addresses in a list.
+This plugin blocks MAIL_FROM addresses in a list.
+
+NOTE: If all you need is to deny mail based on the exact address, this plugin
+will work just fine. If you want to customize the deny message, add blocks
+based on a regex, or add whitelists, please use the mail_from.access plugin.
 
 Configuration
 -------------
---
+++ plugins/mail_from.access.js
@@ -0,0 +1,99 @@
+// mail_from.access plugin
+
+exports.register = function() {
+    var i;
+    var config = this.config.get('mail_from.access.ini');
+    this.wl = this.config.get('mail_from.access.whitelist', 'list');
+    this.bl = this.config.get('mail_from.access.blacklist', 'list');
+    this.deny_msg = config.general && (config.general['deny_msg'] ||
+        'Connection rejected.');
+    var white_regex =
+        this.config.get('mail_from.access.whitelist_regex', 'list');
+    var black_regex =
+        this.config.get('mail_from.access.blacklist_regex', 'list');
+
+    if (white_regex.length) {
+        this.wlregex = new RegExp('^(?:' + white_regex.join('|') + ')$', 'i');
+    }
+
+    if (black_regex.length) {
+        this.blregex = new RegExp('^(?:' + black_regex.join('|') + ')$', 'i');
+    }
+
+    this.register_hook('connect', 'rdns_access');
+}
+
+exports.rdns_access = function(next, connection, params) {
+    var plugin = this;
+    var mail_from = params[0].address();
+
+    // address whitelist checks
+    if (mail_from) {
+        connection.logdebug(plugin, 'checking ' + mail_from +
+            ' against mail_from.access.whitelist');
+
+        if (_in_whitelist(connection, plugin, mail_from)) {
+            connection.logdebug(plugin, "Allowing " + mail_from);
+            return next();
+        }
+    }
+
+    // address blacklist checks
+    if (mail_from) {
+        connection.logdebug(plugin, 'checking ' + mail_from +
+            ' against mail_from.access.blacklist');
+
+        if (_in_blacklist(connection, plugin, mail_from)) {
+            connection.logdebug(plugin, "Rejecting, matched: " + mail_from);
+            return next(DENY, mail_from + ' ' + plugin.deny_msg);
+        }
+    }
+
+    return next();
+}
+
+function _in_whitelist(connection, plugin, address) {
+    var i;
+    for (i in plugin.wl) {
+        connection.logdebug(plugin, 'checking ' + address + ' against ' +
+            plugin.wl[i]);
+
+        if (plugin.wl[i] === address) {
+            return 1;
+        }
+    }
+
+    if (plugin.wlregex) {
+        connection.logdebug(plugin, 'checking ' + address + ' against ' +
+            plugin.wlregex.source);
+
+        if (address.match(plugin.wlregex)) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+function _in_blacklist(connection, plugin, address) {
+    var i;
+    for (i in plugin.bl) {
+        connection.logdebug(plugin, 'checking ' + address + ' against ' +
+            plugin.bl[i]);
+
+        if (plugin.bl[i] === address) {
+            return 1;
+        }
+    }
+
+    if (plugin.blregex) {
+        connection.logdebug(plugin, 'checking ' + address + ' against ' +
+            plugin.blregex.source);
+
+        if (address.match(plugin.blregex)) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
