d7344853318bc09afee43eeb8567d528761e1c42
Eric Day <eday@oddments.org>
2012-04-05 00:55:35 -0700

    Mostly through conn_base conversion to generic smtp_client.

--- plugins/queue/conn_pool_base.js
+++ plugins/queue/conn_pool_base.js
@@ -1,357 +1,258 @@
-// Base queue plugin.
-// This cannot be used on its own. You need to inherit from it.
-// See plugins/queue/smtp_forward.js for an example.
-
-var sock = require('./line_socket');
+var events = require('events');
+var util = require('util');
+var line_socket = require('../../line_socket');
+var config = require('../../config');
+var logger = require('../../logger');
+var generic_pool = require('generic-pool');
 var smtp_regexp = /^([0-9]{3})([ -])(.*)/;
 
-exports.conn_get = function (connection, host, port, timeout) {
-    var conn = {};
-    host = (host) ? host : 'localhost';
-    port = (port) ? port : 25;
-    timeout = (timeout || timeout == 0) ? timeout : 300;
-    conn.pool_name = host + ':' + port + ':' + timeout;
-
-    if (!connection) {
-        throw new Error("Invalid Arguments");
-    }
-
-    if (connection.server.notes.conn_pool &&
-        connection.server.notes.conn_pool[conn.pool_name] &&
-        connection.server.notes.conn_pool[conn.pool_name].length) {
-        connection.logdebug(this, "using connection from the pool: (" +
-            connection.server.notes.conn_pool[conn.pool_name].length + ")");
-
-        conn = connection.server.notes.conn_pool[conn.pool_name].shift();
-
-        // We should just reset these things when we shift a connection off
-        // since we have to setup stuff based on _this_ connection.
-        conn.pool_connection = true;
-
-        // Cleanup all old event listeners
-        // Note, if new ones are added in the caller, please remove them here.
-        conn.socket.removeAllListeners('error');
-        conn.socket.removeAllListeners('timeout');
-        conn.socket.removeAllListeners('close');
-        conn.socket.removeAllListeners('connect');
-        conn.socket.removeAllListeners('line');
-        conn.socket.removeAllListeners('drain');
-
-        var self = this;
-        conn.socket.on('error', function (err) {
-            self.conn_destroy(connection, conn);
-        });
-
-        conn.socket.on('timeout', function () {
-            self.conn_destroy(connection, conn);
-        });
-
-        conn.socket.on('close', function (had_error) {
-            self.conn_destroy(connection, conn);
-        });
-    }
-    else {
-        conn.socket = sock.connect(port, host);
-        conn.socket.setTimeout(timeout * 1000);
-        conn.pool_connection = false;
-    }
-
-    conn.response = [];
-    connection.notes.conn = conn;
-
-    if (connection.server.notes.active_conections >= 0) {
-        connection.server.notes.active_conections++;
-    }
-    else {
-        connection.server.notes.active_conections = 1;
-    }
-
-    connection.logdebug(this, "active connections: (" +
-        connection.server.notes.active_conections + ")");
-
-    return conn;
-}
-
-// function will destroy an conn and pull it out of the idle array
-exports.conn_destroy = function (connection, conn) {
-    var reset_active_connections = 0;
-
-    if (!connection || !conn) {
-        throw new Error("Invalid Arguments");
-    }
-
-    if (conn && conn.socket) {
-        connection.logdebug(this, "destroying connection");
-        conn.socket.destroySoon();
-        conn.socket = 0;
-        reset_active_connections = 1;
-    }
-
-    // Unlink the connection from the proxy just in case we got here
-    // without that happening already.
-    if (connection && connection.notes.conn) {
-        delete connection.notes.conn;
-    }
-
-    if (connection.server.notes.conn_pool &&
-        connection.server.notes.conn_pool[conn.pool_name]) {
-        // Pull that conn from the proxy pool.
-        // Note we do not do this operation that often.
-        var index = connection.server.notes.conn_pool[conn.pool_name].indexOf(conn);
-        if (index != -1) {
-            // if we are pulling something from the proxy pool, it is not
-            // acttive.  This means we do not want to reset it.
-            reset_active_connections = 0;
-            connection.server.notes.conn_pool[conn.pool_name].splice(index, 1);
-            connection.logdebug(this, "pulling dead connection from pool: (" +
-                connection.server.notes.conn_pool[conn.pool_name].length + ")");
-        }
-    }
-
-    if (reset_active_connections &&
-        connection.server.notes.active_conections) {
-        connection.server.notes.active_conections--;
-        connection.logdebug(this, "active connections: (" +
-            connection.server.notes.active_conections + ")");
-    }
-
-    return;
-}
-
-exports.conn_idle = function (connection) {
-    if (!connection) {
-        throw new Error("Invalid Arguments");
-    }
-
-    var conn = connection.notes.conn;
-
-    if (!(conn)) {
-        return;
-    }
-
-    if (connection.server.notes.conn_pool) {
-        if (connection.server.notes.conn_pool[conn.pool_name]) {
-            connection.server.notes.conn_pool[conn.pool_name].push(conn);
-        }
-        else {
-            connection.server.notes.conn_pool[conn.pool_name] = [ conn ];
-        }
-    }
-    else {
-        connection.server.notes.conn_pool = {}
-        connection.server.notes.conn_pool[conn.pool_name] = [ conn ];
-    }
-
-    connection.server.notes.active_conections--;
-
-    connection.logdebug(this, "putting connection back in pool: (" +
-        connection.server.notes.conn_pool[conn.pool_name].length + ")");
-    connection.logdebug(this, "active connections: (" +
-        connection.server.notes.active_conections + ")");
-
-    // Unlink this connection from the proxy now that it is back
-    // in the pool.
-    if (connection && connection.notes.conn) {
-        delete connection.notes.conn;
-    }
-
-    return;
-}
-
-exports.smtp_conn_get = function (connection, host, port, timeout, enable_tls) {
-    var smtp_conn = this.conn_get(connection, host, port, timeout);
-    smtp_conn.data_marker = 0;
-    smtp_conn.dot_pending = true;
-    smtp_conn.response = []
-    smtp_conn.command = 'connect';
+function SMTPClient(port, host, timeout, enable_tls) {
+    events.EventEmitter.call(this);
+    this.socket = line_socket.connect(port, host);
+    this.socket.setTimeout(timeout * 1000);
+    this.released = true;
+    this.set_idle_listeners();
+    this.command = 'greeting';
+    this.response = []
+    this.connected = false;
+    this.data = [];
+    this.data_marker = 0;
+    this.dot_pending = true;
     var self = this;
 
-    smtp_conn.send_data = function () {
-        while (smtp_conn.data_marker < connection.transaction.data_lines.length) {
-            var line = connection.transaction.data_lines[smtp_conn.data_marker];
-            smtp_conn.data_marker++;
-            var wrote_all = smtp_conn.socket.write(line.replace(/^\./, '..').replace(/\r?\n/g, '\r\n'));
-            if (!wrote_all) return;
-        }
-        // we get here if wrote_all still true, and we got to end of data_lines
-        if (smtp_conn.dot_pending) {
-            smtp_conn.dot_pending = false;
-            smtp_conn.send_command('dot');
-        }
-    }
-
-    smtp_conn.send_command = function (cmd, data) {
-        var line = cmd + (data ? (' ' + data) : '');
-        if (cmd === 'dot') {
-            line = '.';
-        }
-        connection.logprotocol(self, "C: " + line);
-        smtp_conn.command = cmd.toLowerCase();
-        smtp_conn.response = [];
-        smtp_conn.socket.write(line + "\r\n");
-    };
-
-    smtp_conn.reset = function () {
-        smtp_conn.socket.removeAllListeners('line');
-        smtp_conn.socket.on('line', function (line) {
-            var matches = smtp_regexp.exec(line);
-            if (!matches) {
-                connection.logerror(self, "Unrecognised response from upstream server: " + line);
-                smtp_conn.socket.end();
-                return;
-            }
-
-            var code = matches[1], cont = matches[2];
-            if (cont !== ' ') {
-                return;
-            }
-
-            if (code.match(/^[45]/)) {
-                smtp_conn.socket.end();
-                return;
-            }
-            self.conn_idle(connection);
-        });
-        smtp_conn.send_command('RSET');
-    };
-
-    smtp_conn.call_next = function (retval, msg) {
-        if (smtp_conn.next) {
-            smtp_conn.next(retval, msg);
-            smtp_conn.next = undefined;
-        }
-    };
-
-    smtp_conn.start = function () {
-        if (smtp_conn.pool_connection) {
-            if (smtp_conn.xclient) {
-                smtp_conn.send_command('XCLIENT', 'ADDR=' + connection.remote_ip);
-            }
-            else {
-                smtp_conn.send_command('MAIL', 'FROM:' + connection.transaction.mail_from);
-            }
-        }
-    };
-
-    smtp_conn.socket.on('drain', function() {
-        if (smtp_conn.dot_pending && smtp_conn.command === 'mailbody') {
-            process.nextTick(function () { smtp_conn.send_data() });
+    this.socket.on('drain', function () {
+        if (self.dot_pending && self.command === 'mailbody') {
+            process.nextTick(function () { self.send_data() });
         }
     });
 
-    smtp_conn.socket.on('error', function (err) {
-        connection.logdebug(self, "Ongoing connection failed: " + err);
-        smtp_conn.call_next();
-    });
-
-    smtp_conn.socket.on('timeout', function () {
-        connection.logdebug(self, "Ongoing connection timed out");
-        smtp_conn.call_next();
-    });
-
-    smtp_conn.socket.on('close', function (had_error) {
-        connection.logdebug(self, "Ongoing connection closed");
-    });
-
-    smtp_conn.socket.on('connect', function () {
-        connection.logdebug(self, "Ongoing connection established");
-    });
-
-    smtp_conn.socket.on('line', function (line) {
-        connection.logprotocol(self, "S: " + line);
+    this.socket.on('line', function (line) {
+        this.emit('server_protocol', line);
         var matches = smtp_regexp.exec(line);
         if (!matches) {
-            connection.logerror(self, "Unrecognised response from upstream server: " + line);
-            smtp_conn.socket.end();
-            return smtp_conn.call_next();
+            self.emit('error', 'Unrecognised response from upstream server: ' + line);
+            self.pool.destroy(this);
+            return;
         }
 
         var code = matches[1],
             cont = matches[2],
-            rest = matches[3];
-        smtp_conn.response.push(rest);
+            msg = matches[3];
+        self.response.push(msg);
         if (cont !== ' ') {
             return;
         }
 
-        if (smtp_conn.command === 'ehlo') {
+        if (self.command === 'ehlo') {
             if (code.match(/^5/)) {
                 // Handle fallback to HELO if EHLO is rejected
-                if (smtp_conn.xclient) {
-                    smtp_conn.send_command('HELO', connection.hello_host);
-                }
-                else {
-                    smtp_conn.send_command('HELO', self.config.get('me'));
-                }
+                self.emit('greeting', 'HELO');
                 return;
             }
             // Parse CAPABILITIES
-            for (var i in smtp_conn.response) {
-                if (smtp_conn.response[i].match(/^XCLIENT/)) {
-                    if(!smtp_conn.xclient) {
-                        // Just use the ADDR= key for now
-                        smtp_conn.send_command('XCLIENT', 'ADDR=' + connection.remote_ip);
+            for (var i in self.response) {
+                if (self.response[i].match(/^XCLIENT/)) {
+                    if(!self.xclient) {
+                        self.send_xclient();
                         return;
                     }
                 }
-                if (smtp_conn.response[i].match(/^STARTTLS/)) {
-                    var key = self.config.get('tls_key.pem', 'data').join("\n");
-                    var cert = self.config.get('tls_cert.pem', 'data').join("\n");
-                    if (key && cert && (!/(true|yes|1)/i.exec(enable_tls))) {
-                        smtp_conn.socket.on('secure', function () {
-                            smtp_conn.send_command('EHLO', self.config.get('me'));
+                if (self.response[i].match(/^STARTTLS/)) {
+                    var key = config.get('tls_key.pem', 'data').join("\n");
+                    var cert = config.get('tls_cert.pem', 'data').join("\n");
+                    if (key && cert && enable_tls) {
+                        this.on('secure', function () {
+                            self.emit('greeting', 'EHLO');
                         });
-                        smtp_conn.send_command('STARTTLS');
+                        self.send_command('STARTTLS');
                         return;
                     }
                 }
             }
         }
-        if (smtp_conn.command === 'xclient' && code.match(/^5/)) {
+        if (self.command === 'xclient' && code.match(/^5/)) {
             // XCLIENT command was rejected (no permission?)
             // Carry on without XCLIENT
-            smtp_conn.command = 'helo';
+            self.command = 'helo';
         }
         else if (code.match(/^[45]/)) {
-            if (!smtp_conn.on_error(code)) {
+            self.emit('bad_code', code, msg);
+            if (self.released) {
                 return;
             }
         }
-        switch (smtp_conn.command) {
+        switch (self.command) {
             case 'xclient':
-                smtp_conn.xclient = true;
-                smtp_conn.send_command('EHLO', connection.hello_host);
+                self.xclient = true;
+                self.emit('xclient', 'EHLO');
                 break;
             case 'starttls':
-                var tls_options = { key: key, cert: cert };
-                smtp_conn.socket.upgrade(tls_options);
+                this.upgrade({key: key, cert: cert});
                 break;
-            case 'connect':
-                smtp_conn.send_command('EHLO', self.config.get('me'));
+            case 'greeting':
+                self.connected = true;
+                self.emit('greeting', 'EHLO');
                 break;
             case 'ehlo':
+                self.emit('helo');
+                break;
             case 'helo':
-                smtp_conn.send_command('MAIL', 'FROM:' + connection.transaction.mail_from);
-                break;
             case 'mail':
-                smtp_conn.on_mail();
-                break;
             case 'rcpt':
-                smtp_conn.on_rcpt();
-                break;
             case 'data':
-                smtp_conn.on_data();
+            case 'rset':
+                self.emit(self.command);
                 break;
             case 'dot':
-                // Return the response from the server back to the client
-                // but add in our transaction UUID at the end of the line.
-                smtp_conn.call_next(OK, smtp_conn.response + ' (' + connection.transaction.uuid + ')');
-                smtp_conn.reset();
+                self.emit('dot');
+                self.data = [];
+                self.data_marker = 0;
+                self.dot_pending = true;
+                break;
+            case 'quit':
+                self.pool.destroy(self);
                 break;
             default:
-                throw new Error("Unknown command: " + smtp_conn.command);
+                throw new Error("Unknown command: " + self.command);
         }
     });
-
-    return smtp_conn;
 }
+
+util.inherits(SMTPClient, events.EventEmitter);
+
+SMTPClient.prototype.start = function () {
+    var self = this;
+
+    this.socket.on('error', function (err) {
+        self.emit('error', 'SMTP connection failed: ' + err);
+        self.pool.destroy(self);
+    });
+
+    this.socket.on('timeout', function () {
+        self.emit('error', 'SMTP connection timed out');
+        self.pool.destroy(self);
+    });
+
+    this.socket.on('close', function (had_error) {
+        self.pool.destroy(self);
+    });
+
+    if (this.connected) {
+        if (this.xclient) {
+            this.send_xclient();
+        }
+        else {
+            this.emit('helo');
+        }
+    }
+};
+
+SMTPClient.prototype.send_command = function (command, data) {
+    var line = (command == 'dot') ? '.' : command + (data ? (' ' + data) : '');
+    this.emit('client_protocol', line);
+    this.command = command.toLowerCase();
+    this.response = [];
+    this.socket.write(line + "\r\n");
+};
+
+SMTPClient.prototype.send_data = function () {
+    while (this.data_marker < this.data.length) {
+        var line = this.data[this.data_marker];
+        this.data_marker++;
+        line = line.replace(/^\./, '..').replace(/\r?\n/g, '\r\n');
+        if (!this.socket.write(line)) {
+            return;
+        }
+    }
+    if (this.dot_pending) {
+        this.dot_pending = false;
+        this.send_command('dot');
+    }
+};
+
+SMTPClient.prototype.release = function () {
+    this.released = true;
+    this.removeAllListeners();
+
+    this.on('bad_code', function (code, msg) {
+        this.pool.destroy(this);
+    });
+
+    this.on('rset', function () {
+        this.removeAllListeners();
+        this.set_idle_listeners();
+        this.pool.release(this);
+    });
+
+    this.send_command('RSET');
+};
+
+SMTPClient.prototype.set_idle_listeners = function () {
+    this.socket.removeAllListeners('error');
+    this.socket.removeAllListeners('timeout');
+    this.socket.removeAllListeners('close');
+    var self = this;
+
+    this.socket.on('error', function (err) {
+        self.dead = true;
+    });
+
+    this.socket.on('timeout', function () {
+        self.dead = true;
+    });
+
+    this.socket.on('close', function (had_error) {
+        self.dead = true;
+    });
+};
+
+exports.get_pool = function (server, config) {
+    var port = config.main.port || 25;
+    var host = config.main.host || 'localhost';
+    var timeout = (config.main.timeout == undefined) ? 300 : config.main.timeout;
+    var enable_tls = /(true|yes|1)/i.exec(config.main.enable_tls) != null;
+    var name = port + ':' + host + ':' + timeout + ':' + enable_tls;
+    if (!server.notes.pool) {
+        server.notes.pool = {};
+    }
+    if (!server.notes.pool[name]) {
+        var pool = generic_pool.Pool({
+            name: name,
+            create: function (callback) {
+                var smtp_client = new SMTPClient(port, host, timeout, enable_tls);
+                callback(null, smtp_client);
+            },
+            destroy: function(smtp_client) {
+                if (!smtp_client.dead) {
+                    smtp_client.socket.destroy();
+                }
+            },
+            max: config.main.max_connections || 1000,
+            idleTimeoutMillis: timeout * 1000,
+            log: function (str, level) {
+                level = (level == 'verbose') ? 'debug' : level;
+                logger['log' + level]('[smtp_client_pool] ' + str);
+            }
+        });
+
+        var acquire = pool.acquire;
+        pool.acquire = function (callback, priority) {
+            var callback_wrapper = function (err, smtp_client) {
+                if (smtp_client.dead) {
+                    pool.acquire(callback, priority);
+                    return;
+                }
+                smtp_client.pool = pool;
+                smtp_client.released = false;
+                callback(err, smtp_client);
+            };
+            acquire.call(pool, callback_wrapper, priority);
+        };
+        server.notes.pool[name] = pool;
+    }
+    return server.notes.pool[name];
+};
+
+exports.get_client = function (server, config, callback) {
+    exports.get_pool(server, config).acquire(callback);
+};
--- plugins/queue/smtp_proxy.js
+++ plugins/queue/smtp_proxy.js
@@ -3,71 +3,121 @@
 // and passes back any errors seen on the ongoing server to the
 // originating server.
 
-var smtp_regexp = /^([0-9]{3})([ -])(.*)/;
-
-exports.register = function () {
-    this.inherits('queue/conn_pool_base');
-}
+var smtp_client_module = require('./plugins/queue/conn_pool_base');
 
 exports.hook_mail = function (next, connection, params) {
     var config = this.config.get('smtp_proxy.ini');
     connection.loginfo(this, "proxying to " + config.main.host + ":" + config.main.port);
-    var smtp_conn = this.smtp_conn_get(connection, config.main.host, config.main.port, config.main.timeout, config.main.enable_tls);
-    smtp_conn.next = next;
-
     var self = this;
+    smtp_client_module.get_client(connection.server, config, function (err, smtp_client) {
+        connection.notes.smtp_client = smtp_client;
+        smtp_client.next = next;
+        smtp_client.call_next = function (retval, msg) {
+            if (this.next) {
+                this.next(retval, msg);
+                delete this.next;
+            }
+        };
 
-    smtp_conn.on_error = function (code) {
-        var response_array = smtp_conn.response.slice();
-        if (smtp_conn.command !== 'rcpt') {
-            // errors are OK for rcpt, but nothing else
-            // this can also happen if the destination server
-            // times out, but that is okay.
-            connection.loginfo(self, "message denied, proxying failed");
-            smtp_conn.reset();
-        }
+        smtp_client.on('client_protocol', function (line) {
+            connection.logprotocol('C: ' + line);
+        });
 
-        smtp_conn.call_next(code.match(/^4/) ? DENYSOFT : DENY, response_array);
-        return false;
-    };
+        smtp_client.on('server_protocol', function (line) {
+            connection.logprotocol('S: ' + line);
+        });
 
-    smtp_conn.on_mail = smtp_conn.on_rcpt = smtp_conn.on_data = function () {
-        smtp_conn.call_next();
-    }
+        var helo = function (command) {
+            if (smtp_client.xclient) {
+                smtp_client.send_command(command, connection.hello_host);
+            }
+            else {
+                smtp_client.send_command(command, self.config.get('me'));
+            }
+        };
 
-    smtp_conn.start();
+        smtp_client.on('greeting', helo);
+        smtp_client.on('xclient', helo);
+
+        smtp_client.send_xclient = function () {
+            smtp_client.send_command('XCLIENT', 'ADDR=' + connection.remote_ip);
+        };
+
+        smtp_client.on('helo', function () {
+            smtp_client.send_command('MAIL', 'FROM:' + connection.transaction.mail_from);
+        });
+
+        smtp_client.on('mail', smtp_client.call_next);
+        smtp_client.on('rcpt', smtp_client.call_next);
+        smtp_client.on('data', smtp_client.call_next);
+
+        smtp_client.on('dot', function () {
+            smtp_client.call_next(OK, smtp_client.response + ' (' + connection.transaction.uuid + ')');
+            smtp_client.release();
+            delete connection.notes.smtp_client;
+        });
+
+        smtp_client.on('error', function (msg) {
+            connection.logerror(msg);
+            smtp_client.call_next();
+        });
+
+        smtp_client.on('bad_code', function (code, msg) {
+            if (smtp_client.command !== 'rcpt') {
+                // errors are OK for rcpt, but nothing else
+                // this can also happen if the destination server
+                // times out, but that is okay.
+                connection.loginfo(self, "message denied, proxying failed");
+                smtp_client.release();
+                delete connection.notes.smtp_client;
+            }
+
+            smtp_client.call_next(code.match(/^4/) ? DENYSOFT : DENY,
+                smtp_client.response.slice());
+        });
+
+        smtp_client.start();
+    });
 };
 
 exports.hook_rcpt_ok = function (next, connection, recipient) {
-    var smtp_conn = connection.notes.conn;
-    smtp_conn.next = next;
-    smtp_conn.send_command('RCPT', 'TO:' + recipient);
+    var smtp_client = connection.notes.smtp_client;
+    if (!smtp_client) return next();
+    smtp_client.next = next;
+    smtp_client.send_command('RCPT', 'TO:' + recipient);
 };
 
 exports.hook_data = function (next, connection) {
-    var smtp_conn = connection.notes.conn;
-    smtp_conn.next = next;
-    smtp_conn.send_command("DATA");
+    var smtp_client = connection.notes.smtp_client;
+    if (!smtp_client) return next();
+    smtp_client.next = next;
+    smtp_client.send_command("DATA");
 };
 
 exports.hook_queue = function (next, connection) {
-    var smtp_conn = connection.notes.conn;
-    smtp_conn.command = 'mailbody';
-    smtp_conn.next = next;
-    smtp_conn.send_data();
+    var smtp_client = connection.notes.smtp_client;
+    if (!smtp_client) return next();
+    smtp_client.command = 'mailbody';
+    smtp_client.next = next;
+    smtp_client.data = connection.transaction.data_lines;
+    smtp_client.send_data();
 };
 
 exports.hook_rset = function (next, connection) {
-    var smtp_conn = connection.notes.conn;
-    smtp_conn.reset();
+    var smtp_client = connection.notes.smtp_client;
+    if (!smtp_client) return next();
+    smtp_client.release();
+    delete connection.notes.smtp_client;
     next();
 }
 
 exports.hook_quit = exports.hook_rset;
 
 exports.hook_disconnect = function (next, connection) {
-    var smtp_conn = connection.notes.conn;
-    smtp_conn.reset();
-    smtp_conn.call_next();
+    var smtp_client = connection.notes.smtp_client;
+    if (!smtp_client) return next();
+    smtp_client.release();
+    delete connection.notes.smtp_client;
+    smtp_client.call_next();
     next();
 };
