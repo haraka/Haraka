800955127caf3c1da08e88e33571bbba5dc2e45b
Steve Freegard <steve.freegard@fsl.com>
2012-05-12 07:35:07 +0000

    Request TLS peer certificates and report status on STARTTLS

--- tls_socket.js
+++ tls_socket.js
@@ -158,15 +158,14 @@ function createServer(cb) {
 
         var socket = new pluggableStream(cryptoSocket);
 
-        socket.upgrade = function (options) {
+        socket.upgrade = function (options, cb) {
             log.logdebug("Upgrading to TLS");
             
             socket.clean();
             cryptoSocket.removeAllListeners('data');
             var sslcontext = crypto.createCredentials(options);
 
-            var pair = tls.createSecurePair(sslcontext, true, false, false);
-
+            var pair = tls.createSecurePair(sslcontext, true, true, false);
             var cleartext = pipe(pair, cryptoSocket);
 
             pair.on('error', function(exception) {
@@ -183,8 +182,10 @@ function createServer(cb) {
                 } else {
                     cleartext.authorized = true;
                 }
+                var cert = pair.cleartext.getPeerCertificate();
 
                 socket.emit('secure');
+                if (cb) cb(cleartext.authorized, verifyError, cert);
             });
 
             cleartext._controlReleased = true;
