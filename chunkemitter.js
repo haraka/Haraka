8f968c46d75a28ac1cbd575634d799183ecc790b
Matt Sergeant <helpme+github@gmail.com>
2012-11-13 10:29:30 -0500

    Support node v0.6 by adding in Buffer.concat

--- chunkemitter.js
+++ chunkemitter.js
@@ -12,6 +12,37 @@ function ChunkEmitter(buffer_size) {
 
 util.inherits(ChunkEmitter, EventEmitter);
 
+if (!Buffer.concat) {
+    Buffer.concat = function(list, length) {
+      if (!Array.isArray(list)) {
+        throw new Error('Usage: Buffer.concat(list, [length])');
+      }
+
+      if (list.length === 0) {
+        return new Buffer(0);
+      } else if (list.length === 1) {
+        return list[0];
+      }
+
+      if (typeof length !== 'number') {
+        length = 0;
+        for (var i = 0; i < list.length; i++) {
+          var buf = list[i];
+          length += buf.length;
+        }
+      }
+
+      var buffer = new Buffer(length);
+      var pos = 0;
+      for (var i = 0; i < list.length; i++) {
+        var buf = list[i];
+        buf.copy(buffer, pos);
+        pos += buf.length;
+      }
+      return buffer;
+    };
+}
+
 ChunkEmitter.prototype.fill = function (input) {
     if (typeof input === 'string') {
         input = new Buffer(input);
