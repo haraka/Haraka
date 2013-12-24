6cd520166b6472680ad4ecf26cb86fd030b2cd87
Matt Simerson <matt@tnpi.net>
2013-12-24 18:02:28 -0500

    rfc5322_header: allow config file overrides

--- plugins/data.rfc5322_header_checks.js
+++ plugins/data.rfc5322_header_checks.js
@@ -1,8 +1,29 @@
-// Enforce RFC 5322 Section 3.6 
+
+// Enforce RFC 5322 Section 3.6
 var required_headers = ['Date', 'From'];
-var singular_headers =  ['Date', 'From', 'Sender', 'Reply-To', 'To', 'Cc',
+var singular_headers = ['Date', 'From', 'Sender', 'Reply-To', 'To', 'Cc',
                          'Bcc', 'Message-Id', 'In-Reply-To', 'References',
                          'Subject'];
+var date_future_days = 2;
+var date_past_days   = 15;
+
+exports.register = function() {
+    var config   = this.config.get('data.headers.ini');
+
+    if ( config.main.required ) {
+        required_headers = config.main.required.split(',');
+    };
+    if ( config.main.singular ) {
+        singular_headers = config.main.singular.split(',');
+    };
+
+    if ( config.main.date_future_days ) {
+        date_future_days = config.main.date_future_days;
+    }
+    if ( config.main.date_past_days ) {
+        date_past_days = config.main.date_past_days;
+    }
+}
 
 exports.hook_data_post = function (next, connection) {
     var header = connection.transaction.header;
@@ -10,7 +31,7 @@ exports.hook_data_post = function (next, connection) {
     for (var i=0,l=required_headers.length; i < l; i++) {
         if (header.get_all(required_headers[i]).length === 0)
         {
-            return next(DENY, "Required header '" + required_headers[i] + 
+            return next(DENY, "Required header '" + required_headers[i] +
                                 "' missing");
         }
     }
@@ -18,8 +39,8 @@ exports.hook_data_post = function (next, connection) {
     // Headers that MUST be unique if present
     for (var i=0,l=singular_headers.length; i < l; i++) {
         if (header.get_all(singular_headers[i]).length > 1) {
-             return next(DENY, "Message contains non-unique '" +
-                                singular_headers[i] + "' header");
+             return next(DENY, "Only one " + singular_headers[i] +
+                " header allowed. See RFC 5322, Section 3.6");
         }
     }
 
