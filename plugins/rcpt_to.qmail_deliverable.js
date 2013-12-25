8d35d366b8548cb23aa23f9aae4e4f2005bfba28
Matt Simerson <matt@tnpi.net>
2013-12-24 19:06:09 -0500

    qmd: code quality improvements
    removed no-longer-needed breaks
    changed error message for unknown QMD::Client error
    pass this, so error messages log with plugin name

--- plugins/rcpt_to.qmail_deliverable.js
+++ plugins/rcpt_to.qmail_deliverable.js
@@ -27,96 +27,81 @@ exports.rcpt_to_qmd = function(next, connection, params) {
     // but Haraka might have done this for us, by this point
 
     this.logdebug("checking " + email );
-    return get_qmd_response(next,connection,email);
+    return get_qmd_response(next,this,connection,email);
 }
 
-function get_qmd_response(next,conn,email) {
+function get_qmd_response(next,plugin,connection,email) {
     options.path = '/qd1/deliverable?' + email;
-    conn.logprotocol(conn, 'PATH: ' + options.path);
+    plugin.logprotocol('PATH: ' + options.path);
     var req = http.get(options, function(res) {
-        conn.logprotocol(conn, 'STATUS: ' + res.statusCode);
-        conn.logprotocol(conn, 'HEADERS: ' + JSON.stringify(res.headers));
+        plugin.logprotocol('STATUS: ' + res.statusCode);
+        plugin.logprotocol('HEADERS: ' + JSON.stringify(res.headers));
         res.on('data', function (chunk) {
             res.setEncoding('utf8');
-            conn.logprotocol(conn, 'BODY: ' + chunk);
+            plugin.logprotocol('BODY: ' + chunk);
             var hexnum = new Number(chunk).toString(16);
-            return check_qmd_reponse( next, conn, hexnum );
+            return check_qmd_reponse(next,plugin,connection,hexnum);
         });
     }).on('error', function(e) {
-        conn.loginfo(conn,"Got error: " + e.message);
+        plugin.loginfo("Got error: " + e.message);
     });
 };
 
-function check_qmd_reponse(next,connection,hexnum) {
-    connection.logprotocol( "HEXRV: " + hexnum );
+function check_qmd_reponse(next,plugin,connection,hexnum) {
+    plugin.logprotocol("HEXRV: " + hexnum );
 
     switch(hexnum) {
         case '11':
-            connection.loginfo("qmd error, permission failure");
+            plugin.loginfo("error, permission failure");
             return next();
-            break;
         case '12':
-            connection.loginfo("qmd pass, qmail-command in dot-qmail");
+            plugin.loginfo("pass, qmail-command in dot-qmail");
             return next(OK);
-            break;
         case '13':
-            connection.loginfo("qmd pass, bouncesaying with program");
+            plugin.loginfo("pass, bouncesaying with program");
             return next(OK);
-            break;
         case '14':
             var from = connection.transaction.mail_from.address();
             if ( ! from || from === '<>') {
                 return next(DENY, "fail, mailing lists do not accept null senders");
             }
-            connection.loginfo("qmd pass, ezmlm list");
+            plugin.loginfo("pass, ezmlm list");
             return next(OK);
-            break;
         case '21':
-            connection.loginfo("qmd Temporarily undeliverable: group/world writable");
+            plugin.loginfo("Temporarily undeliverable: group/world writable");
             return next();
-            break;
         case '22':
-            connection.loginfo("qmd Temporarily undeliverable: sticky home directory");
+            plugin.loginfo("Temporarily undeliverable: sticky home directory");
             return next();
-            break;
         case '2f':
-            connection.loginfo("qmd error, Qmail::Deliverable::Client::ERROR");
+            plugin.loginfo("error communicating with qmail-deliverabled.");
             return next();
-            break;
         case 'f1':
-            connection.loginfo("qmd pass, normal delivery");
+            plugin.loginfo("pass, normal delivery");
             return next(OK);
-            break;
         case 'f2':
-            connection.loginfo("qmd pass, vpopmail dir");
+            plugin.loginfo("pass, vpopmail dir");
             return next(OK);
-            break;
         case 'f3':
-            connection.loginfo("qmd pass, vpopmail alias");
+            plugin.loginfo("pass, vpopmail alias");
             return next(OK);
-            break;
         case 'f4':
-            connection.loginfo("qmd pass, vpopmail catchall");
+            plugin.loginfo("pass, vpopmail catchall");
             return next(OK);
-            break;
         case 'f5':
-            connection.loginfo("qmd pass, vpopmail vuser");
+            plugin.loginfo("pass, vpopmail vuser");
             return next(OK);
-            break;
         case 'f6':
-            connection.loginfo("qmd pass, vpopmail qmail-ext");
+            plugin.loginfo("pass, vpopmail qmail-ext");
             return next(OK);
-            break;
         case 'fe':
-            connection.loginfo("qmd error, SHOULD NOT HAPPEN");
+            plugin.loginfo("error, SHOULD NOT HAPPEN");
             return next();
-            break;
         case 'ff':
-            connection.loginfo("qmd fail, address not local");
+            plugin.loginfo("fail, address not local");
             return next();
-            break;
         default:
-            connection.loginfo("qmd error, unknown rv: " + hexnum);
+            plugin.loginfo("error, unknown rv: " + hexnum);
             return next();
     }
 };
