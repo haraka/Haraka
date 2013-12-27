a604da0b58fded6a228ccf19d6ace09e1311813a
Matt Simerson <matt@tnpi.net>
2013-12-27 17:07:15 -0500

    restored rfc5322_header_checks

---
+++ plugins/data.rfc5322_header_checks.js
@@ -0,0 +1,27 @@
+// Enforce RFC 5322 Section 3.6 
+var required_headers = ['Date', 'From'];
+var singular_headers =  ['Date', 'From', 'Sender', 'Reply-To', 'To', 'Cc',
+                         'Bcc', 'Message-Id', 'In-Reply-To', 'References',
+                         'Subject'];
+
+exports.hook_data_post = function (next, connection) {
+    var header = connection.transaction.header;
+    // Headers that MUST be present
+    for (var i=0,l=required_headers.length; i < l; i++) {
+        if (header.get_all(required_headers[i]).length === 0)
+        {
+            return next(DENY, "Required header '" + required_headers[i] + 
+                                "' missing");
+        }
+    }
+
+    // Headers that MUST be unique if present
+    for (var i=0,l=singular_headers.length; i < l; i++) {
+        if (header.get_all(singular_headers[i]).length > 1) {
+             return next(DENY, "Message contains non-unique '" +
+                                singular_headers[i] + "' header");
+        }
+    }
+
+    return next();
+}
