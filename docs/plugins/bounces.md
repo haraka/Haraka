1bdc895daa2c690b0bb50feadfcc538cbc116934
Matt Simerson <matt@tnpi.net>
2014-01-19 02:39:29 -0500

    bounce: initial authoring

---
+++ docs/plugins/bounces.md
@@ -0,0 +1,23 @@
+bounce
+===================
+This plugin provides options for bounce processing.
+
+
+Configuration
+-------------------
+
+- reject_all
+
+Blocks all bounce messages using the simple rule of checking
+for `MAIL FROM:<>`.
+
+This is useful to enable if you have a mail server that gets spoofed too
+much but very few legitimate users. It is potentially bad to block all
+bounce messages, but unfortunately for some hosts, sometimes necessary.
+
+
+- reject_invalid
+--------------------
+This option tries to assure the message really is a bounce. It makes
+sure the message has a single recipient and that the return path is
+empty.
---
+++ plugins/bounce.js
@@ -0,0 +1,59 @@
+// bounce tests
+
+exports.register = function () {
+    var plugin = this;
+
+    this.register_hook('mail',         'bounce_mail');
+    // this.register_hook('rcpt',         'bounce_rcpt');
+    // this.register_hook('data',         'bounce_data');
+    this.register_hook('data_post',    'bounce_data_post');
+};
+
+exports.bounce_mail = function (next, connection, params) {
+    var mail_from = params[0];
+    if (!mail_from.isNull()) return next();    // not a bounce
+    var cfg = this.config.get('bounce.ini');
+    if (cfg.reject_all)
+        return next(DENY, "No bounces accepted here");
+    return next();
+}
+
+exports.bounce_data_post = function(next, connection) {
+    var plugin = connection;
+
+    if (!has_null_sender(connection)) return next(); // not a bounce.
+
+    var cfg = this.config.get('bounce.ini');
+    var rej = cfg.reject_invalid;
+
+    // Valid bounces have a single recipient
+    var err = has_single_recipient(connection, plugin);
+    if (err && rej) return next(DENY, err);
+
+    // validate that Return-Path is empty, RFC 3834
+    err = has_empty_return_path(connection, plugin);
+    if (err && rej) return next(DENY, err);
+
+    return next();
+};
+
+function has_empty_return_path(connection, plugin) {
+    var rp = connection.transaction.header.get('Return-Path');
+    if (!rp) return;
+    if (rp === '<>') return;
+    connection.loginfo(plugin, "bounce messages must not have a Return-Path");
+    return "a bounce return path must be empty (RFC 3834)";
+};
+
+function has_single_recipient(connection, plugin) {
+    if (connection.transaction.rcpt_to.length === 1) return;
+
+    connection.loginfo(plugin, "bogus bounce to: " + 
+        connection.transaction.rcpt_to.join(','));
+
+    return "this bounce message does not have 1 recipient";
+};
+
+function has_null_sender(connection) {
+    return connection.transaction.mail_from.isNull() ? true : false;
+};
