954f86cfce14bc03fbc969d3dff046f9ede1c4fa
Matt Simerson <matt@tnpi.net>
2014-01-20 04:01:05 -0500

    geoip: added distance calculations

--- plugins/connect.geoip.js
+++ plugins/connect.geoip.js
@@ -1,34 +1,21 @@
 var geoip = require('geoip-lite');
 var net = require('net');
 
+var local_ip, local_geoip;
+
 exports.hook_connect = function (next, connection) {
+    var plugin = this;
     connection.notes.geoip = geoip.lookup(connection.remote_ip);
 
     if (!connection.notes.geoip) return next();
 
     var cfg = this.config.get('connect.geoip.ini');
-    connection.loginfo(this, get_results(connection, cfg));
+    calculate_distance(plugin, connection, cfg);
+
+    connection.loginfo(plugin, get_results(connection, cfg));
 
     return next();
 }
-
-function get_results(connection, cfg) {
-    var r = connection.notes.geoip;
-    if (!r) return '';
-
-    // geoip.lookup results look like this:
-    // range: [ 3479299040, 3479299071 ],
-    //    country: 'US',
-    //    region: 'CA',
-    //    city: 'San Francisco',
-    //    ll: [37.7484, -122.4156]
-
-    var show = [ r.country ];
-    if ( r.region && cfg.main.show_region) show.push(r.region);
-    if ( r.city   && cfg.main.show_city  ) show.push(r.city);
-
-    return show.join(', ');
-};
 
 exports.hook_data_post = function (next, connection) {
     var txn = connection.transaction;
@@ -53,6 +40,97 @@ exports.hook_data_post = function (next, connection) {
         txn.add_header('X-Haraka-GeoIP-Received', received.join(' '));
     }
     return next();
+};
+
+function calculate_distance(plugin, connection, cfg) {
+    if (!cfg.main.calc_distance) return;
+
+    if (!local_ip) { local_ip = cfg.main.public_ip; };
+    if (!local_ip) { local_ip = connection.local_ip; };
+    if (!local_ip) return;
+
+    if (!local_geoip) {
+        local_geoip = geoip.lookup(local_ip)
+        connection.loginfo(plugin, "local_geoip set: "+local_geoip);
+    };
+    if (!local_geoip) return;
+
+//  two formulas for calculating Great Circle Distance. Perhaps worth
+//  benchmarking them?
+
+//  var gcd = geodatasource(local_geoip.ll[0], local_geoip.ll[1],
+//      connection.notes.geoip.ll[0], connection.notes.geoip.ll[1]);
+
+    var gcd = haversine(local_geoip.ll[0], local_geoip.ll[1],
+        connection.notes.geoip.ll[0], connection.notes.geoip.ll[1]);
+
+    connection.notes.geoip.distance = gcd;
+};
+
+function haversine(lat1, lon1, lat2, lon2) {
+    var R = 6371; // km
+    function toRad(v) { return v * Math.PI / 180; };
+    var dLat = toRad(lat2-lat1);
+    var dLon = toRad(lon2-lon1);
+    var lat1 = toRad(lat1);
+    var lat2 = toRad(lat2);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d.toFixed(0);
+}
+
+function geodatasource(lat1, lon1, lat2, lon2) {
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::  From: http://www.geodatasource.com/developers/javascript
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points).
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2014            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var radlon1 = Math.PI * lon1/180;
+    var radlon2 = Math.PI * lon2/180;
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;
+    return dist.toFixed(0);
+}
+
+function get_results(connection, cfg) {
+    var r = connection.notes.geoip;
+    if (!r) return '';
+
+    // geoip.lookup results look like this:
+    // range: [ 3479299040, 3479299071 ],
+    //    country: 'US',
+    //    region: 'CA',
+    //    city: 'San Francisco',
+    //    ll: [37.7484, -122.4156]
+
+    var show = [ r.country ];
+    if ( r.region && cfg.main.show_region) show.push(r.region);
+    if ( r.city   && cfg.main.show_city  ) show.push(r.city);
+    if ( r.distance && cfg.main.calc_distance ) show.push(r.distance+'km');
+
+    return show.join(', ');
 };
 
 function user_agent(connection, plugin) {
