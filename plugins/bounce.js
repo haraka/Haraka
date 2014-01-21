793d5b3a2e698dd4f36079da30ec2806185c5f32
Matt Simerson <matt@tnpi.net>
2014-01-21 00:33:40 -0500

    restored heavily commented and disabled data_from
    explaining why Return-Path checking isn't enabled

--- plugins/bounce.js
+++ plugins/bounce.js
@@ -3,6 +3,7 @@
 exports.register = function () {
     this.register_hook('mail',         'bounce_mail');
     this.register_hook('data',         'bounce_data');
+//  this.register_hook('data_post',    'bounce_data_post');
 };
 
 exports.bounce_mail = function (next, connection, params) {
@@ -15,7 +16,6 @@ exports.bounce_mail = function (next, connection, params) {
 
 exports.bounce_data = function(next, connection) {
     var plugin = connection;
-
     if (!has_null_sender(connection.transaction.mail_from)) return next();
 
     var cfg = this.config.get('bounce.ini');
@@ -27,12 +27,39 @@ exports.bounce_data = function(next, connection) {
     return next();
 };
 
+exports.bounce_data_post = function(next, connection) {
+    return next();
+
+    // Bounce messages generally do not have a Return-Path set. This checks
+    // for that. But whether it should is worth questioning...
+
+    // On Jan 20, 2014, Matt Simerson examined the most recent 50,000 mail
+    // connections for the presence of Return-Path in bounce messages. I
+    // found 14 hits, 12 of which were from Google, in response to
+    // undeliverable DMARC reports (IE, automated messages that Google
+    // shouldn't have replied to). Another appears to be a valid bounce from
+    // a poorly configured mailer, and the 14th was a confirmed spam kill.
+    // Unless new data demonstrate otherwise, this should remain disabled.
+
+    // Return-Path, aka Reverse-PATH, Envelope FROM, RFC5321.MailFrom
+    // validate that the Return-Path header is empty, RFC 3834
+
+    if (!has_null_sender(connection.transaction.mail_from)) return next();
+    var plugin = connection;
+    var rp = connection.transaction.header.get('Return-Path');
+    if (rp && rp !== '<>') {
+        connection.loginfo(plugin, "bounce with non-empty Return-Path");
+        return next(DENY, "bounce with non-empty Return-Path (RFC 3834)");
+    };
+    return next();
+};
+
 function has_single_recipient(connection, plugin) {
     if (connection.transaction.rcpt_to.length === 1) return;
 
     // Valid bounces have a single recipient
-    connection.loginfo(plugin, "bogus bounce to: " + 
-        connection.transaction.rcpt_to.join(','));
+    connection.loginfo(plugin, "bounce with too many recipients to: "
+        + connection.transaction.rcpt_to.join(','));
 
     connection.transaction.notes.bounce='invalid';
     return "this bounce message does not have 1 recipient";
