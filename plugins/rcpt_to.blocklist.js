f80840e475ffd7432e2eb4f28e87d94c2103018e
Matt Simerson <matt@tnpi.net>
2014-01-25 23:35:46 -0500

    TODO: added bounce rename, deprecate *blocklist
    deprecate the mail_from and rcpt_to.blocklist plugins (replaced by *.access)
    rename bounce -> data.bounce

--- plugins/rcpt_to.blocklist.js
+++ plugins/rcpt_to.blocklist.js
@@ -1,6 +1,10 @@
 // Block mail from matching anything in the list
 var utils = require('./utils');
 
+exports.register = function () {
+    this.logwarn("NOTICE: plugin deprecated, use 'rcpt_to.access' instead!");
+}
+
 exports.hook_rcpt = function (next, connection, params) {
     var rcpt_to = params[0].address();
     var list = this.config.get('rcpt_to.blocklist', 'list');
