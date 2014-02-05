e357a35f56cd0a52e079127fbadb9e033bc18435
Matt Sergeant <helpme@gmail.com>
2014-02-05 14:22:58 -0500

    Write config get options documentation

--- config.js
+++ config.js
@@ -7,11 +7,24 @@ var config = exports;
 
 var config_path = process.env.HARAKA ? path.join(process.env.HARAKA, 'config') : path.join(__dirname, './config');
 
+/* Ways this can be called:
+config.get('thing');
+config.get('thing', type);
+config.get('thing', cb);
+config.get('thing', type, cb);
+config.get('thing', type, options);
+config.get('thing', type, cb, options);
+*/
 config.get = function(name, type, cb, options) {
-    if (type === 'nolog') {
-        type = arguments[2]; // deprecated - TODO: remove later
+    if (typeof type == 'function') {
+        options = cb;
+        cb = type;
+        type = 'value';
     }
-
+    if (typeof cb != 'function') {
+        options = cb;
+        cb = null;
+    }
     type = type || 'value';
     var full_path = path.resolve(config_path, name);
     var results = configloader.read_config(full_path, type, cb, options); 
