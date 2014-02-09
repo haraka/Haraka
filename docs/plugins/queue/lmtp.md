0618422e430fe572cd9fffe46bfdce01998c9268
Peter Janotta <git.xfvz@mapiverse.de.vu>
2014-02-09 11:59:41 +0100

    Changed queue/lmtp plugin to read config file instead of in-built parameters + documentation

--- docs/plugins/queue/lmtp.md
+++ docs/plugins/queue/lmtp.md
@@ -1,9 +1,19 @@
 queue/lmtp
 ========
 
-Describe what your plugin does here.
+This plugin delivers mails to inbound domains via LMTP/SMTP.
 
 Configuration
 -------------
 
-* `config/some_file` - describe what effect this config file has
+* `config/delivery_domains`
+    This config file provides server address and port of LMTP/SMTP server to deliver for specific inbound domains
+    
+    Example:
+    
+    [example.com]
+    priority=0
+    exchange=127.0.0.1
+    port=24
+    isLMTP=1    #isLMTP=0 assumes SMTP
+    
--- plugins/queue/lmtp.js
+++ plugins/queue/lmtp.js
@@ -1,16 +1,15 @@
+//queue/lmtp
+
 "use strict";
 
 var outbound = require('./outbound');
 
-//Should be changed to config lookup 
-var lmtp_domain = 'laptop-workstation';
-var lmtp_server_address = '127.0.0.1';
-var lmtp_server_port = "24"
-
 //get_mx hook
 exports.hook_get_mx = function (next, hmail, domain) {
-    if (domain == lmtp_domain) {
-        next(OK, [{priority:0, exchange:lmtp_server_address, port:lmtp_server_port, isLMTP:true}]);
+    //TODO: allow arrays of mx objects; currently only one mx object per domain
+    var domains_ini = this.config.get('delivery_domains.ini', 'ini');
+    if (domain in domains_ini) {
+        next(OK, domains_ini[domain]);
     }
     else {
         next(CONT);
