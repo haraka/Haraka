dc1722fd8b79fb5c02f89fa14e47134463b60a5b
Matt Simerson <matt@tnpi.net>
2014-02-13 12:21:09 -0500

    asn: replace 'note.js' with 'results'

--- plugins/connect.asn.js
+++ plugins/connect.asn.js
@@ -4,7 +4,6 @@ var dns = require('dns');
 var providers = [];
 
 exports.register = function () {
-    this.inherits('note');
     var config = this.config.get('connect.asn.ini');
     if (config.main.providers) {
         providers = config.main.providers.split(/[\s,;]+/);
@@ -19,7 +18,6 @@ exports.register = function () {
 
 exports.hook_lookup_rdns = function (next, connection) {
     var plugin = this;
-    plugin.note_init({conn: connection, plugin: this});
     var ip = connection.remote_ip;
 
     for (var i=0; i < providers.length; i++) {
@@ -55,22 +53,24 @@ exports.hook_lookup_rdns = function (next, connection) {
 };
 
 exports.parse_routeviews = function (str, connection) {
+    var plugin = this;
     var r = str.split(/ /);
     if ( r.length !== 3 ) {
         connection.loginfo("result length not 3: " + r.length + ",string length: " + str.length);
         return '';
     }
-    this.note({conn: connection, asn: { asn: r[0], net: r[1] }, emit: true});
+    connection.results.add(plugin, {asn: {asn: r[0], net: r[1] }, emit: true});
     return r[0];
 };
 
 exports.parse_cymru = function (str, connection) {
+    var plugin = this;
     var r = str.split(/\s+\|\s+/);
     //  99.177.75.208.origin.asn.cymru.com. 14350 IN TXT "40431 | 208.75.176.0/21 | US | arin | 2007-03-02"
     if ( r.length !== 5 ) {
         connection.loginfo("result length not 3: " + r.length + ",string length: " + str.length);
         return '';
     }
-    this.note({conn: connection, asn: r[0], net: r[1], country: r[2], authority: r[3], emit: true});
+    connection.results.add(plugin, {asn: r[0], net: r[1], country: r[2], authority: r[3], emit: true});
     return r[0];
 };
