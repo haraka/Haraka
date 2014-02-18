e758d881ed55fe2d719832c787577474b48ef8da
Matt Simerson <matt@tnpi.net>
2014-02-18 01:52:24 -0500

    asn: s/logprotocol/logdebug/

--- plugins/connect.asn.js
+++ plugins/connect.asn.js
@@ -10,9 +10,9 @@ exports.register = function () {
     }
     else {
         providers = [ 'origin.asn.cymru.com' ];
-        // TODO: test after 3/1/2014 and see if bug is fixed
-        // providers.push('asn.routeviews.org');
         // broken due to TXT handling bug in node.js
+        // providers.push('asn.routeviews.org');
+        // TODO: test after 3/1/2014 and see if bug is fixed
     }
 };
 
@@ -22,10 +22,10 @@ exports.hook_lookup_rdns = function (next, connection) {
 
     for (var i=0; i < providers.length; i++) {
         var zone = providers[i];
-        connection.logprotocol(plugin, "zone: " + zone);
+        // connection.logdebug(plugin, "zone: " + zone);
 
         var query = ip.split('.').reverse().join('.') + '.' + zone;
-        connection.logprotocol(plugin, "query: " + query);
+        // connection.logdebug(plugin, "query: " + query);
 
         dns.resolveTxt(query, function (err, addrs) {
             if (err) {
@@ -35,7 +35,6 @@ exports.hook_lookup_rdns = function (next, connection) {
 
             for (var i=0; i < addrs.length; i++) {
                 connection.logdebug(plugin, zone + " answer: " + addrs[i]);
-                var asn;
                 if (zone === 'origin.asn.cymru.com') {
                     plugin.parse_cymru(addrs[i], connection);
                 }
@@ -43,7 +42,7 @@ exports.hook_lookup_rdns = function (next, connection) {
                     plugin.parse_routeviews(addrs[i], connection);
                 }
                 else {
-                    throw "unrecognized ASN provider: " + zone;
+                    connection.logerror(plugin, "unrecognized ASN provider: " + zone);
                 }
             }
         });
@@ -56,7 +55,7 @@ exports.parse_routeviews = function (str, connection) {
     var plugin = this;
     var r = str.split(/ /);
     if ( r.length !== 3 ) {
-        connection.loginfo("result length not 3: " + r.length + ",string length: " + str.length);
+        connection.logerror("result length not 3: " + r.length + ",string length: " + str.length);
         return '';
     }
     connection.results.add(plugin, {asn: {asn: r[0], net: r[1] }, emit: true});
@@ -68,7 +67,7 @@ exports.parse_cymru = function (str, connection) {
     var r = str.split(/\s+\|\s+/);
     //  99.177.75.208.origin.asn.cymru.com. 14350 IN TXT "40431 | 208.75.176.0/21 | US | arin | 2007-03-02"
     if ( r.length !== 5 ) {
-        connection.loginfo("result length not 3: " + r.length + ",string length: " + str.length);
+        connection.logerror("result length not 3: " + r.length + ",string length: " + str.length);
         return '';
     }
     connection.results.add(plugin, {asn: r[0], net: r[1], country: r[2], authority: r[3], emit: true});
