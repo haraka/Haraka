2e2e16a14a2a287902ef5845be72110236455819
Matt Simerson <matt@tnpi.net>
2014-02-25 03:18:24 -0500

    spf: added results. refactored to remove duplication

--- plugins/spf.js
+++ plugins/spf.js
@@ -7,17 +7,17 @@ var net_utils = require('./net_utils');
 var plugin = exports;
 SPF.prototype.log_debug = function (str) {
     return plugin.logdebug(str);
-}
+};
 
 exports.hook_helo = exports.hook_ehlo = function (next, connection, helo) {
+    var plugin = this;
     // Bypass private IPs
     if (net_utils.is_rfc1918(connection.remote_ip)) return next();
-    var self = this;
     var timeout = false;
     var spf = new SPF();
     var timer = setTimeout(function () {
         timeout = true;
-        connection.logerror(self, 'timeout');
+        connection.logerror(plugin, 'timeout');
         return next();
     }, 30 * 1000);
     spf.hello_host = helo;
@@ -25,86 +25,46 @@ exports.hook_helo = exports.hook_ehlo = function (next, connection, helo) {
         if (timer) clearTimeout(timer);
         if (timeout) return;
         if (err) {
-            connection.logerror(self, err);
+            connection.logerror(plugin, err);
             return next();
         }
-        connection.loginfo(self, [
-            'identity=helo',
-            'ip=' + connection.remote_ip,
-            'domain="' + connection.hello_host + '"',
-            'mfrom=<' + 'postmaster@' + connection.hello_host + '>',
-            'result=' + spf.result(result),
-            ].join(' '));
-        connection.notes.spf_helo = result;
+        var host = connection.hello_host;
+        plugin.log_result(connection, 'helo', host, 'postmaster@' + host, spf.result(result));
+
+        connection.notes.spf_helo = result;  // used between hooks
+        connection.results.add(plugin, {
+            scope: 'helo',
+            result: spf.result(result),
+            domain: host,
+        });
         return next();
     });
-}
+};
 
 exports.hook_mail = function (next, connection, params) {
+    var plugin = this;
+    // Bypass private IPs
+    if (net_utils.is_rfc1918(connection.remote_ip)) return next();
+
     var txn = connection.transaction;
     if (!txn) return next();
 
-    // Bypass private IPs
-    if (net_utils.is_rfc1918(connection.remote_ip)) return next();
     var cfg = this.config.get('spf.ini');
 
-    var self = this;
     var mfrom = params[0].address();
     var host = params[0].host;
     var spf = new SPF();
-    var auth_result;
 
     if (connection.notes.spf_helo) {
-        auth_result = spf.result(connection.notes.spf_helo).toLowerCase();
-        // Add a trace header
-        txn.add_leading_header('Received-SPF', 
-            spf.result(connection.notes.spf_helo) +
-            ' (' + self.config.get('me') + ': domain of ' + connection.hello_host +
-            ((connection.notes.spf_helo === spf.SPF_PASS) ? ' designates ' : ' does not designate ') +
-            connection.remote_ip + ' as permitted sender) ' + [
-                'receiver=' + self.config.get('me'),
-                'identity=helo',
-                'client-ip=' + connection.remote_ip,
-                'helo=' + connection.hello_host,
-                'envelope-from=<' + mfrom + '>',
-            ].join('; '));
-        // Use the result from HELO if the return-path is null
-        if (!host) {
-            connection.auth_results( "spf="+auth_result+" smtp.helo="+connection.hello_host);
-            switch (connection.notes.spf_helo) {
-                case spf.SPF_NONE:
-                case spf.SPF_NEUTRAL:
-                case spf.SPF_PASS:
-                    return next();
-                case spf.SPF_SOFTFAIL:
-                    if (cfg.main.helo_softfail_reject) {
-                        return next(DENY, 'sender <> via ' + connection.hello_host +
-                                          ' SPF SoftFail');
-                    }
-                    return next();
-                case spf.SPF_FAIL:
-                    if (cfg.main.helo_fail_reject) {
-                        return next(DENY, 'sender <> via ' + connection.hello_host + 
-                                          ' SPF Fail');
-                    }
-                    return next();
-                case spf.SPF_TEMPERROR:
-                    if (cfg.main.helo_temperror_defer) {
-                        return next(DENYSOFT, 'sender <> via ' + connection.hello_host + 
-                                              ' SPF Temporary Error');
-                    }
-                    return next();
-                case spf.SPF_PERMERROR:
-                    if (cfg.main.helo_permerror_reject) {
-                        return next(DENY, 'sender <> via ' + connection.hello_host + 
-                                          ' SPF Permanent Error');
-                    }
-                    return next();
-                default:
-                    // Unknown result
-                    connection.logerror(self, 'unknown result code=' + result);
-                    return next();
-            }
+        var h_result = connection.notes.spf_helo;
+        var h_host = connection.hello_host;
+        plugin.save_to_header(connection, spf, h_result, mfrom, h_host, 'helo');
+        if (!host) {   // Use results from HELO if the return-path is null
+            var auth_result = spf.result(h_result).toLowerCase();
+            connection.auth_results( "spf="+auth_result+" smtp.helo=" + h_host);
+
+            var sender = '<> via ' + h_host;
+            return plugin.return_results(next, connection, spf, 'helo', h_result, sender);
         }
     }
 
@@ -113,7 +73,7 @@ exports.hook_mail = function (next, connection, params) {
     var timeout = false;
     var timer = setTimeout(function () {
         timeout = true;
-        connection.logerror(self, 'timeout');
+        connection.logerror(plugin, 'timeout');
         return next();
     }, 30 * 1000);
 
@@ -122,61 +82,90 @@ exports.hook_mail = function (next, connection, params) {
         if (timer) clearTimeout(timer);
         if (timeout) return;
         if (err) {
-            connection.logerror(self, err);
+            connection.logerror(plugin, err);
             return next();
         }
-        connection.loginfo(self, [
-            'identity=mfrom',
-            'ip=' + connection.remote_ip,
-            'domain="' + host + '"',
-            'mfrom=<' + mfrom + '>',
-            'result=' + spf.result(result)
-            ].join(' '));
-        // Add a trace header
-        txn.add_leading_header('Received-SPF', 
-            spf.result(result) +
-            ' (' + self.config.get('me') + ': domain of ' + host +
-            ((result === spf.SPF_PASS) ? ' designates ' : ' does not designate ') +
-            connection.remote_ip + ' as permitted sender) ' + [
-                'receiver=' + self.config.get('me'),
-                'identity=mailfrom',
-                'client-ip=' + connection.remote_ip,
-                'helo=' + connection.hello_host,
-                'envelope-from=<' + mfrom + '>',
-            ].join('; '));
-        auth_result = spf.result(result).toLowerCase();
+        plugin.log_result(connection, 'mfrom', host, mfrom, spf.result(result));
+        plugin.save_to_header(connection, spf, result, mfrom, host, 'mailfrom');
+
+        var auth_result = spf.result(result).toLowerCase();
         connection.auth_results( "spf="+auth_result+" smtp.mailfrom="+host);
+
         txn.notes.spf_mail_result = spf.result(result);
         txn.notes.spf_mail_record = spf.spf_record;
-        switch (result) {
-            case spf.SPF_NONE:
-            case spf.SPF_NEUTRAL:
-            case spf.SPF_PASS:
-                return next();
-            case spf.SPF_SOFTFAIL:
-                if (cfg.main.mail_softfail_reject) {
-                    return next(DENY, 'sender <' + mfrom + '> SPF SoftFail');
-                }
-                return next();
-            case spf.SPF_FAIL:
-                if (cfg.main.mail_fail_reject) {
-                    return next(DENY, 'sender <' + mfrom + '> SPF Fail');
-                }
-                return next();
-            case spf.SPF_TEMPERROR:
-                if (cfg.main.mail_temperror_defer) {
-                    return next(DENYSOFT, 'sender <' + mfrom + '> SPF Temporary Error');
-                }
-                return next();
-            case spf.SPF_PERMERROR:
-                if (cfg.main.mail_permerror_reject) {
-                    return next(DENY, 'sender <' + mfrom + '> SPF Permanent Error');
-                }
-                return next();
-            default:
-                // Unknown result
-                connection.logerror(self, 'unknown result code=' + result);
-                return next();
+        var spf_note = {
+            scope: 'mfrom',
+            result: spf.result(result),
+            domain: host,
+        };
+        if (connection.transaction) {
+            connection.transaction.results.add(plugin, spf_note);
         }
+        return plugin.return_results(next, connection, spf, 'mail', result, '<'+mfrom+'>');
     });
-}
+};
+
+exports.log_result = function (connection, scope, host, mfrom, result) {
+    connection.loginfo(this, [
+        'identity=' + scope,
+        'ip=' + connection.remote_ip,
+        'domain="' + host + '"',
+        'mfrom=<' + mfrom + '>',
+        'result=' + result
+        ].join(' '));
+};
+
+exports.return_results = function(next, connection, spf, scope, result, sender) {
+    var cfg = this.config.get('spf.ini');
+    var msgpre = 'sender ' + sender;
+
+    switch (result) {
+        case spf.SPF_NONE:
+        case spf.SPF_NEUTRAL:
+        case spf.SPF_PASS:
+            return next();
+        case spf.SPF_SOFTFAIL:
+            if (cfg.main[scope + '_softfail_reject']) {
+                return next(DENY, msgpre + ' SPF SoftFail');
+            }
+            return next();
+        case spf.SPF_FAIL:
+            if (cfg.main[scope + '_fail_reject']) {
+                return next(DENY, msgpre + ' SPF Fail');
+            }
+            return next();
+        case spf.SPF_TEMPERROR:
+            if (cfg.main[scope + '_temperror_defer']) {
+                return next(DENYSOFT, msgpre + ' SPF Temporary Error');
+            }
+            return next();
+        case spf.SPF_PERMERROR:
+            if (cfg.main[scope + '_permerror_reject']) {
+                return next(DENY, msgpre + ' SPF Permanent Error');
+            }
+            return next();
+        default:
+            // Unknown result
+            connection.logerror(self, 'unknown result code=' + result);
+            return next();
+    }
+};
+
+exports.save_to_header = function (connection, spf, result, mfrom, host, id) {
+    var plugin = this;
+    // Add a trace header
+    if (!connection) return;
+    if (!connection.transaction) return;
+    connection.transaction.add_leading_header('Received-SPF',
+        spf.result(result) +
+        ' (' + plugin.config.get('me') + ': domain of ' + host +
+        ((result === spf.SPF_PASS) ? ' designates ' : ' does not designate ') +
+        connection.remote_ip + ' as permitted sender) ' + [
+            'receiver=' + plugin.config.get('me'),
+            'identity=' + id,
+            'client-ip=' + connection.remote_ip,
+            'helo=' + connection.hello_host,
+            'envelope-from=<' + mfrom + '>'
+        ].join('; ')
+    );
+};
