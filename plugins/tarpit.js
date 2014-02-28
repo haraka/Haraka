ae433df00297bd19faac68fc0a982b18b7538cfd
Matt Simerson <matt@tnpi.net>
2014-02-28 16:55:31 -0500

    tarpit: added ability to configure hooks_to_delay

--- plugins/tarpit.js
+++ plugins/tarpit.js
@@ -1,33 +1,40 @@
 // tarpit
 
+var hooks_to_delay = ['connect', 'helo', 'ehlo', 'mail', 'rcpt', 'rcpt_ok', 'data',
+   'data_post', 'queue', 'unrecognized_command', 'vrfy', 'noop', 'rset', 'quit'];
+
 exports.register = function () {
     // Register tarpit function last
-    var self = this;
-    ['connect', 'helo', 'ehlo', 'mail', 'rcpt', 'rcpt_ok', 'data',
-     'data_post', 'queue', 'unrecognized_command', 'vrfy', 'noop', 
-     'rset', 'quit'].forEach(function (hook) {
-        self.register_hook(hook, 'tarpit');
-    });
-}
+    var plugin = this;
+
+    var cfg = plugin.config.get('tarpit.ini');
+    if (cfg && cfg.main.hooks_to_delay) {
+        hooks_to_delay = cfg.main.hooks_to_delay.split(/[\s,;]+/);
+    }
+
+    for (var i=0; i < hooks_to_delay.length; i++) {
+        var hook = hooks_to_delay[i];
+        plugin.register_hook(hook, 'tarpit');
+    }
+};
 
 exports.tarpit = function (next, connection) {
-    var transaction = connection.transaction;
+    var plugin = this;
+    if (!connection) return next();
+
     var conn_delay, trans_delay;
-    if (transaction && transaction.notes) {
-        trans_delay = transaction.notes.tarpit;
+    if (connection.transaction) {
+        trans_delay = connection.transaction.notes.tarpit;
     }
-    if (connection && connection.notes) {
+    if (connection) {
         conn_delay = connection.notes.tarpit;
     }
+
     var delay = trans_delay || conn_delay;
-    if (delay) {
-        connection.loginfo(this, 'tarpitting response for ' + delay + 's');
-        setTimeout(function () {
-            // Only return if we still have a connection...
-            if (connection) return next();
-        }, (delay*1000));
-    }
-    else {
-        return next();
-    }
-}
+    if (!delay) return next();
+
+    connection.loginfo(plugin, 'tarpitting response for ' + delay + 's');
+    setTimeout(function () {
+        if (connection) return next();
+    },  delay * 1000);
+};
