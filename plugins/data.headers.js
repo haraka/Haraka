15fb852f74cd3f6c3c2379ec52b4b8b344da2941
Matt Simerson <matt@tnpi.net>
2014-03-12 21:02:16 -0400

    headers: refresh config once per connection
    declare config.main.reject as boolean
    added from_matches results

--- plugins/data.headers.js
+++ plugins/data.headers.js
@@ -1,4 +1,6 @@
 // validate message headers and some fields
+var net_utils = require('./net_utils');
+var reject;
 
 exports.register = function () {
     this.register_hook('data_post', 'duplicate_singular');
@@ -7,15 +9,28 @@ exports.register = function () {
     this.register_hook('data_post', 'invalid_return_path');
     this.register_hook('data_post', 'user_agent');
     this.register_hook('data_post', 'direct_to_mx');
+    this.register_hook('data_post', 'from_matches');
 };
+
+exports.hook_data = function(next, connection) {
+    // refresh when a connection makes it to data
+    this.cfg = this.config.get('data.headers.ini', {
+        booleans: ['main.reject'],
+    });
+
+    if (this.cfg.main.reject !== undefined) {
+        reject = this.cfg.main.reject;
+    }
+
+    return next();
+}
 
 exports.duplicate_singular = function(next, connection) {
     var plugin = this;
-    var config = plugin.config.get('data.headers.ini');
 
     // RFC 5322 Section 3.6, Headers that MUST be unique if present
-    var singular = config.main.singular !== 'undefined' ?
-                   config.main.singular.split(',') :
+    var singular = plugin.cfg.main.singular !== undefined ?
+                   plugin.cfg.main.singular.split(',') :
                    ['Date', 'From', 'Sender', 'Reply-To', 'To', 'Cc',
                     'Bcc', 'Message-Id', 'In-Reply-To', 'References',
                     'Subject'];
@@ -30,8 +45,11 @@ exports.duplicate_singular = function(next, connection) {
     }
 
     if (failures.length) {
-        return next(DENY, "Only one " + failures[0] +
-            " header allowed. See RFC 5322, Section 3.6");
+        if (reject) {
+            return next(DENY, "Only one " + failures[0] +
+                " header allowed. See RFC 5322, Section 3.6");
+        }
+        return next();
     }
 
     connection.transaction.results.add(plugin, {pass: 'duplicate'});
@@ -40,11 +58,10 @@ exports.duplicate_singular = function(next, connection) {
 
 exports.missing_required = function(next, connection) {
     var plugin = this;
-    var config = plugin.config.get('data.headers.ini');
 
     // Enforce RFC 5322 Section 3.6, Headers that MUST be present
-    var required = config.main.required !== 'undefined' ?
-                   config.main.required.split(',') :
+    var required = plugin.cfg.main.required !== undefined ?
+                   plugin.cfg.main.required.split(',') :
                    ['Date', 'From'];
 
     var failures = [];
@@ -55,8 +72,12 @@ exports.missing_required = function(next, connection) {
             failures.push(h);
         }
     }
+
     if (failures.length) {
-        return next(DENY, "Required header '" + failures[0] + "' missing");
+        if (reject) {
+            return next(DENY, "Required header '" + failures[0] + "' missing");
+        }
+        return next();
     }
 
     connection.transaction.results.add(plugin, {pass: 'missing'});
@@ -76,7 +97,10 @@ exports.invalid_return_path = function(next, connection) {
     if (rp) {
         if (connection.relaying) {      // On messages we originate
             connection.transaction.results.add(plugin, {fail: 'Return-Path', emit: true});
-            return next(DENY, "outgoing mail must not have a Return-Path header (RFC 5321)");
+            if (reject) {
+                return next(DENY, "outgoing mail must not have a Return-Path header (RFC 5321)");
+            }
+            return next();
         }
         else {
             // generally, messages from the internet shouldn't have a
@@ -96,15 +120,14 @@ exports.invalid_date = function (next, connection) {
     var plugin = this;
     // Assure Date header value is [somewhat] sane
 
-    var config = plugin.config.get('data.headers.ini');
     var msg_date = connection.transaction.header.get_all('Date');
     if (!msg_date || msg_date.length === 0) return next();
 
     connection.logdebug(plugin, "message date: " + msg_date);
     msg_date = Date.parse(msg_date);
 
-    var date_future_days = config.main.date_future_days !== 'undefined' ?
-                           config.main.date_future_days :
+    var date_future_days = plugin.cfg.main.date_future_days !== undefined ?
+                           plugin.cfg.main.date_future_days :
                            2;
 
     if (date_future_days > 0) {
@@ -113,12 +136,15 @@ exports.invalid_date = function (next, connection) {
         // connection.logdebug(plugin, "too future: " + too_future);
         if (msg_date > too_future) {
             connection.transaction.results.add(plugin, {fail: 'invalid_date(future)'});
-            return next(DENY, "The Date header is too far in the future");
+            if (reject) {
+                return next(DENY, "The Date header is too far in the future");
+            }
+            return next();
         }
     }
 
-    var date_past_days = config.main.date_past_days !== 'undefined' ?
-                         config.main.date_past_days :
+    var date_past_days = plugin.cfg.main.date_past_days !== undefined ?
+                         plugin.cfg.main.date_past_days :
                          15;
 
     if (date_past_days > 0) {
@@ -128,7 +154,10 @@ exports.invalid_date = function (next, connection) {
         if (msg_date < too_old) {
             connection.loginfo(plugin, "date is older than: " + too_old);
             connection.transaction.results.add(plugin, {fail: 'invalid_date(past)'});
-            return next(DENY, "The Date header is too old");
+            if (reject) {
+                return next(DENY, "The Date header is too old");
+            }
+            return next();
         }
     }
 
@@ -154,7 +183,7 @@ exports.user_agent = function (next, connection) {
         var header = connection.transaction.header.get(name);
         if (!header) continue;   // header not present
         found_ua++;
-        connection.transaction.results.add(plugin, {pass: 'UA('+header+')'});
+        connection.transaction.results.add(plugin, {pass: 'UA('+header.substring(0,12)+')'});
     }
     if (found_ua) return next();
 
@@ -192,3 +221,50 @@ exports.direct_to_mx = function (next, connection) {
     connection.transaction.results.add(plugin, {pass: 'direct-to-mx('+c+')'});
     return next();
 };
+
+exports.from_matches = function (next, connection) {
+    var plugin = this;
+    if (!connection) return next();
+    if (!connection.transaction) return next();
+
+    var env_from = connection.transaction.mail_from.address();
+    var hdr_from = connection.transaction.header.get('From');
+    if (!hdr_from) {
+        connection.transaction.results.add(plugin, {fail: 'from_match(missing)'});
+        return next();
+    }
+
+    // From: "Typical User" <user@example.com>
+    var hdr_part = hdr_from.match(/<([\S]+)@([\S]+)>/);
+    if (!hdr_part) {
+        // From: staff@hotmail.com
+        hdr_part = hdr_from.match(/[\s]*([\S]+)@([\S]+)[\s\r\n]*$/);
+        if (!hdr_part) {
+            connection.transaction.results.add(plugin, {fail: 'from_match(regex miss ('+hdr_from+'))'});
+            return next();
+        }
+    }
+
+    var msg_from = hdr_part[1] + '@' + hdr_part[2];
+
+    if (env_from.toLowerCase() == msg_from.toLowerCase()) {
+        connection.transaction.results.add(plugin, {pass: 'from_match'});
+        return next();
+    }
+
+    var env_dom = net_utils.getOrganizationalDomain(connection.transaction.mail_from.host);
+    var msg_dom = net_utils.getOrganizationalDomain(msg_from.replace(/^.*@/,''));
+    if (env_dom.toLowerCase() == msg_dom.toLowerCase()) {
+        connection.transaction.results.add(plugin, {pass: 'from_match(domain)'});
+        return next();
+    }
+/*
+    connection.logdebug(plugin, 'raw from: ' + connection.transaction.mail_from.address());
+    connection.logdebug(plugin, 'header from: .' + hdr_from + '.');
+    connection.logdebug(plugin, 'msg    from: .' + msg_from + '.');
+*/
+    connection.transaction.results.add(plugin, {emit: true,
+        fail: 'from_match(' + env_dom + ' / ' + msg_dom + ')'
+    });
+    return next();
+}
