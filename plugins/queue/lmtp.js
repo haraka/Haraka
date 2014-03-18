f4114f68cb99e8902de9c62b18224a7881e5792b
Steve Freegard <steve.freegard@fsl.com>
2014-03-18 18:00:22 +0000

    Revert "Record accepted domains at hook_rcpt and improve queue/lmtp"
    This reverts commit 362cabfba1d807565a869719cb485633d1948df7.

--- plugins/queue/lmtp.js
+++ plugins/queue/lmtp.js
@@ -1,23 +1,23 @@
-// queue/lmtp
+//queue/lmtp
+
+"use strict";
+
 var outbound = require('./outbound');
 
 exports.hook_get_mx = function (next, hmail, domain) {
-    var ok_domains = hmail.todo.notes.ok_domains;
-    // Only route 'inbound' domains to LMTP
-    if (ok_domains && ok_domains[domain.toLowerCase()]) {
-        var config = this.config.get('lmtp.ini', 'ini');
-        var section = config[domain] || config.main;
-        var mx = {
-            priority: 0,
-            exchange: section.host || '127.0.0.1',
-            port: section.port || 24,
-            using_lmtp: true
-        };
-        return next(OK, mx);
-    }
-    return next();
+    if (!hmail.todo.notes.using_lmtp) return next();
+    var config = this.config.get('lmtp.ini', 'ini');
+    var section = config[domain] || config.main;
+    var mx = {
+        priority: 0,
+        exchange: section.host || '127.0.0.1',
+        port: section.port || 24,
+        using_lmtp: true
+    };
+    return next(OK, mx);
 }
 
 exports.hook_queue = function (next, connection) {
+    connection.transaction.notes.using_lmtp = true;
     outbound.send_email(connection.transaction, next);
 }
