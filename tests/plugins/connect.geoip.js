83bfb0ae9b929ad3414af02be0133c7a71cb7189
Matt Simerson <matt@tnpi.net>
2014-03-29 00:41:13 -0400

    geoip: use net_utils.get_public_ip
    with backwards compat (if locally set)

---
+++ tests/plugins/connect.geoip.js
@@ -0,0 +1,70 @@
+
+var stub             = require('../fixtures/stub'),
+    Connection       = require('../fixtures/stub_connection'),
+    Plugin           = require('../fixtures/stub_plugin'),
+    configfile       = require('../../configfile'),
+    config           = require('../../config'),
+    // Header       = require('../../mailheader').Header,
+    ResultStore      = require("../../result_store");
+
+function _set_up(callback) {
+    this.backup = {};
+
+    this.plugin = Plugin('connect.geoip');
+    this.plugin.config = config;
+    this.plugin.cfg = { main: {} };
+
+    this.connection = Connection.createConnection();
+    this.connection.results = new ResultStore(this.plugin);
+
+    callback();
+}
+function _tear_down(callback) {
+    callback();
+}
+
+// ServedBy ll: [ 47.6738, -122.3419 ],
+// WMISD  [ 38, -97 ]
+
+exports.haversine = {
+    setUp : _set_up,
+    tearDown : _tear_down,
+    'WA to MI': function (test) {
+        test.expect(2);
+        var r = this.plugin.haversine(47.673, -122.3419, 38, -97);
+        test.equal(true, (r > 2000));
+        test.equal(true, (r < 2500));
+        // console.log(r);
+        test.done();
+    }
+};
+
+exports.hook_connect = {
+    setUp : _set_up,
+    tearDown : _tear_down,
+
+    'seattle: ': function (test) {
+        test.expect(2);
+        var cb = function (rc) {
+            test.equal(undefined, rc);
+            var r = this.connection.results.get('connect.geoip');
+            // console.log(r);
+            test.ok(r);
+            test.done();
+        }.bind(this);
+        this.connection.remote_ip='192.48.85.146';
+        this.plugin.hook_connect(cb, this.connection);
+    },
+    'michigan: ': function (test) {
+        test.expect(2);
+        var cb = function (rc) {
+            test.equal(undefined, rc);
+            var r = this.connection.results.get('connect.geoip');
+            // console.log(r);
+            test.ok(r);
+            test.done();
+        }.bind(this);
+        this.connection.remote_ip='199.176.179.3';
+        this.plugin.hook_connect(cb, this.connection);
+    },
+};
