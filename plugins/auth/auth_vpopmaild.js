b91982267b9b531ace686c1c15e24f58ed695be5
Matt Simerson <matt@tnpi.net>
2014-04-25 20:00:37 -0400

    auth_vpopmaild: add tests and CRAM-MD5 auth support

--- plugins/auth/auth_vpopmaild.js
+++ plugins/auth/auth_vpopmaild.js
@@ -1,16 +1,20 @@
 // Auth against vpopmaild
 
-var sock = require('./line_socket');
-var cfg;
+var net    = require('net'),
+    crypto = require('crypto');
 
 exports.register = function () {
     this.inherits('auth/auth_base');
 };
 
 exports.hook_capabilities = function (next, connection) {
-    if (!connection.using_tls) return next();
+    if (!connection.using_tls) { return next(); }
+    var plugin = this;
+    plugin.load_config();
 
     var methods = [ 'PLAIN', 'LOGIN' ];
+    if (plugin.cfg.main.sysadmin) { methods.push('CRAM-MD5'); }
+
     connection.capabilities.push('AUTH ' + methods.join(' '));
     connection.notes.allowed_auth_methods = methods;
 
@@ -18,60 +22,28 @@ exports.hook_capabilities = function (next, connection) {
 };
 
 exports.check_plain_passwd = function (connection, user, passwd, cb) {
-    this.try_auth_vpopmaild(connection, user, passwd, cb);
-};
-
-exports.try_auth_vpopmaild = function (connection, user, passwd, cb) {
     var plugin = this;
-    cfg = plugin.config.get('auth_vpopmaild.ini');
+    plugin.load_config();
 
+    var chunk_count = 0;
     var auth_success = false;
-    var result = "";
-    var ok_count = 0;
-    var port = 89;
-    var host = '127.0.0.1';
 
-    var domain = (user.split('@'))[1];
-    if (domain && cfg[domain]) {
-        if (cfg[domain].port) port = cfg[domain].port;
-        if (cfg[domain].host) host = cfg[domain].host;
-    }
-    else {
-        if (cfg.main.port) port = cfg.main.port;
-        if (cfg.main.host) host = cfg.main.host;
-    }
-    connection.logdebug(plugin, 'sock: ' + host + ':' + port);
+    var socket = plugin.get_vpopmaild_socket(user);
 
-    var socket = new sock.Socket();
-    socket.connect(port, host);
-    socket.setTimeout(300 * 1000);
-
-    socket.on('timeout', function () {
-        connection.logerror(plugin, "vpopmaild connection timed out");
-        socket.end();
-    });
-    socket.on('error', function (err) {
-        connection.logerror(plugin, "vpopmaild connection failed: " + err);
-        socket.end();
-    });
-    socket.on('connect', function () {
-        // wait for server to send us +OK vvvvv
-    });
-    socket.on('line', function (line) {
-        if (line.match(/^\+OK/)) {    // default server response: +OK
-            ok_count++;
-            if (ok_count === 1) {     // first OK is just a 'ready'
+    socket.on('data', function (chunk) {
+        chunk_count++;
+        if (chunk_count === 1) {
+            if (/^\+OK/.test(chunk)) {
                 socket.write("slogin " + user + ' ' + passwd + "\n\r");
+                return;
             }
-            if (ok_count === 2) {     // second OK is response to slogin
+            socket.end();
+        }
+        if (chunk_count === 2) {
+            if (/^\+OK/.test(chunk)) {    // slogin reply
                 auth_success = true;
                 socket.write("quit\n\r");
             }
-        }
-        if (line.match(/^\-ERR/)) {   // auth failed
-            // DANGER, do not say 'goodbye' to the server with "quit\n\r". The
-            // server will respond '+OK', which could be mis-interpreted as an
-            // auth response.
             socket.end();             // disconnect
         }
     });
@@ -81,3 +53,107 @@ exports.try_auth_vpopmaild = function (connection, user, passwd, cb) {
     });
 };
 
+exports.load_config = function () {
+    var plugin = this;
+    if (plugin.cfg) { return; }  // already loaded
+
+    var lc = function () {
+        plugin.loginfo("loading auth_vpopmaild.ini");
+        plugin.cfg = plugin.config.get('auth_vpopmaild.ini', lc);
+    };
+    lc();
+};
+
+exports.get_sock_opts = function (user) {
+    var plugin = this;
+
+    plugin.sock_opts = {
+        port: 89,
+        host: '127.0.0.1',
+        sysadmin: undefined,
+    };
+
+    var domain = (user.split('@'))[1];
+    var sect = plugin.cfg.main;
+    if (domain && plugin.cfg[domain]) {
+        sect = plugin.cfg[domain];
+    }
+
+    if (sect.port)     { plugin.sock_opts.port     = sect.port;     }
+    if (sect.host)     { plugin.sock_opts.host     = sect.host;     }
+    if (sect.sysadmin) { plugin.sock_opts.sysadmin = sect.sysadmin; }
+
+    plugin.logdebug('sock: ' + plugin.sock_opts.host + ':' + plugin.sock_opts.port);
+    return plugin.sock_opts;
+};
+
+exports.get_vpopmaild_socket = function (user) {
+    var plugin = this;
+    plugin.load_config();
+    plugin.get_sock_opts(user);
+
+    var socket = new net.Socket();
+    socket.connect(plugin.sock_opts.port, plugin.sock_opts.host);
+    socket.setTimeout(300 * 1000);
+    socket.setEncoding('utf8');
+
+    socket.on('timeout', function () {
+        plugin.logerror("vpopmaild connection timed out");
+        socket.end();
+    });
+    socket.on('error', function (err) {
+        plugin.logerror("vpopmaild connection failed: " + err);
+        socket.end();
+    });
+    socket.on('connect', function () {
+        plugin.logdebug('vpopmail connected');
+    });
+    return socket;
+};
+
+exports.get_plain_passwd = function (user, cb) {
+    var plugin = this;
+    plugin.load_config();
+
+    var socket = plugin.get_vpopmaild_socket(user);
+    if (!plugin.sock_opts.sysadmin) {
+        plugin.logerror("missing sysadmin credentials");
+        return cb(null);
+    }
+
+    var sys = plugin.sock_opts.sysadmin.split(':');
+    var plain_pass = null;
+    var chunk_count = 0;
+
+    socket.on('data', function (chunk) {
+        chunk_count++;
+        plugin.logdebug(chunk_count + '\t' + chunk);
+        if (chunk_count === 1) {
+            if (/^\+OK/.test(chunk)) {
+                socket.write("slogin " + sys[0] + ' ' + sys[1] + "\n\r");
+                return;
+            }
+            plugin.logerror("no ok to start");
+            socket.end();             // disconnect
+        }
+        // slogin reply
+        if (chunk_count === 2) {
+            if (/^\+OK/.test(chunk)) {
+                plugin.logdebug('login success, getting user info');
+                socket.write("user_info " + user + "\n\r");
+                return;
+            }
+            plugin.logerror("syadmin login failed");
+            socket.end();             // disconnect
+        }
+        if (chunk_count > 2) {
+            if (!/clear_text_password/.test(chunk)) { return; }
+            var pass = chunk.match(/clear_text_password\s(\S+)\s/);
+            plain_pass = pass[1];
+            socket.write("quit\n\r");
+        }
+    });
+    socket.on('end', function () {
+        cb(plain_pass);
+    });
+};
