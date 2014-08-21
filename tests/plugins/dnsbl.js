ddc5bfd5f06205ea70574cc44772f05a50aa5880
Matt Simerson <matt@tnpi.net>
2014-08-20 19:35:04 -0700

    dnsbl: consolidated 'bypass' code into should_skip
    1. remove duplicated code
    2. added test coverage

--- tests/plugins/dnsbl.js
+++ tests/plugins/dnsbl.js
@@ -1,7 +1,8 @@
 var stub         = require('../fixtures/stub'),
     Plugin       = require('../fixtures/stub_plugin'),
     configfile   = require('../../configfile'),
-    config       = require('../../config');
+    config       = require('../../config'),
+    Connection   = require('../fixtures/stub_connection');
 
 function _set_up(callback) {
     this.backup = {};
@@ -48,12 +49,65 @@ exports.get_uniq_zones = {
     'dnsbl.zones': function (test) {
         test.expect(2);
         this.plugin.refresh_config();
-
         this.plugin.cfg.main.zones = 'dnsbl.test, dnsbl2.test';
         this.plugin.get_uniq_zones();
         test.notEqual(-1, this.plugin.zones.indexOf('dnsbl.test'));
         test.notEqual(-1, this.plugin.zones.indexOf('dnsbl2.test'));
 
+        test.done();
+    },
+};
+
+exports.should_skip = {
+    setUp : _set_up,
+    tearDown : _tear_down,
+    'no connection': function (test) {
+        test.expect(1);
+        test.equal(true, this.plugin.should_skip());
+        test.done();
+    },
+    'no remote_ip': function (test) {
+        test.expect(1);
+        this.connection = Connection.createConnection();
+        test.equal(true, this.plugin.should_skip(this.connection));
+        test.done();
+    },
+    'private remote_ip, no zones': function (test) {
+        test.expect(1);
+        this.connection = Connection.createConnection();
+        this.connection.remote_ip = '192.168.1.1';
+        test.equal(true, this.plugin.should_skip(this.connection));
+        test.done();
+    },
+    'private remote_ip': function (test) {
+        test.expect(1);
+        this.connection = Connection.createConnection();
+        this.connection.remote_ip = '192.168.1.1';
+
+        this.plugin.refresh_config();
+        this.plugin.cfg.main.zones = 'dnsbl.test, dnsbl2.test';
+        this.plugin.get_uniq_zones();
+
+        test.equal(true, this.plugin.should_skip(this.connection));
+        test.done();
+    },
+    'public remote_ip': function (test) {
+        test.expect(1);
+        this.connection = Connection.createConnection();
+        this.connection.remote_ip = '208.1.1.1';
+
+        this.plugin.refresh_config();
+        this.plugin.cfg.main.zones = 'dnsbl.test, dnsbl2.test';
+        this.plugin.get_uniq_zones();
+
+        test.equal(false, this.plugin.should_skip(this.connection));
+        test.done();
+    },
+    'public remote_ip, no zones': function (test) {
+        test.expect(1);
+        this.connection = Connection.createConnection();
+        this.connection.remote_ip = '208.1.1.1';
+        test.equal(true, this.plugin.should_skip(this.connection));
         test.done();
     },
 };
