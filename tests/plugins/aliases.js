6d26ae638ad300591a4c8a5aa78fedc354a101d2
Christopher Mooney <chris@dod.net>
2014-08-26 13:38:58 -0400

    reverting this change to how it was
    while testing, I changed this and forgot to change it back.

--- tests/plugins/aliases.js
+++ tests/plugins/aliases.js
@@ -61,7 +61,8 @@ exports.aliases = {
     'register function should inherit from queue/discard' : function (test) {
         test.expect(2);
         test.ok(this.plugin.inherits.called);
-        test.ok(this.plugin.inherits.args[0].match(/queue\/discard/));
+        test.equals(this.plugin.inherits.args[0], 'queue/discard');
+        // test.ok(this.plugin.inherits.args[0].match(/queue\/discard/));
         test.done();
     },
     'register function should call register_hook()' : function (test) {
