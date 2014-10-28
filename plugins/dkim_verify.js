cdb0f6daed5c7b1ad819d8d50dbfee82914f3c53
Matt Simerson <matt@tnpi.net>
2014-10-28 01:13:34 -0700

    dkim: save results to resultStore

--- plugins/dkim_verify.js
+++ plugins/dkim_verify.js
@@ -16,11 +16,11 @@ DKIMVerifyStream.prototype.debug = function (str) {
 };
 
 exports.hook_data_post = function(next, connection) {
-    var self = this;
+    var plugin = this;
     var txn = connection.transaction;
     var verifier = new DKIMVerifyStream(function (err, result, results) {
         if (err) {
-            connection.logerror(self, 'error=' + err);
+            connection.logerror(plugin, 'error=' + err);
         }
         if (!results) return next();
         results.forEach(function (res) {
@@ -29,12 +29,16 @@ exports.hook_data_post = function(next, connection) {
               ((res.error) ? ' (' + res.error + ')' : '') + 
               ' header.i=' + res.identity
             );
-            connection.loginfo(self, 'identity="' + res.identity + '" ' +
+            connection.loginfo(plugin, 'identity="' + res.identity + '" ' +
                                      'domain="' + res.domain + '" ' + 
                                      'result=' + res.result + ' ' +
                                      ((res.error) ? ' (' + res.error + ')' : ''));
+
+            if      (res.result === 'pass') { txn.results.add(plugin, { pass: res.domain }); }
+            else if (res.result === 'fail') { txn.results.add(plugin, { fail: res.domain }); }
+            else                            { txn.results.add(plugin, { err:  res.domain }); }
         });
-        connection.logdebug(self, JSON.stringify(results));
+        connection.logdebug(plugin, JSON.stringify(results));
         // Store results for other plugins
         txn.notes.dkim_results = results;
         return next();
