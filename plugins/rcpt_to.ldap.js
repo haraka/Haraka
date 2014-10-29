03825e76d7775fd91a82152d304ced8fbd16612e
Matt Simerson <matt@tnpi.net>
2014-10-28 21:13:22 -0700

    ldap: ws cleanups, test load ldapjs in register,
    only register hook if ldapjs loads

--- plugins/rcpt_to.ldap.js
+++ plugins/rcpt_to.ldap.js
@@ -1,81 +1,93 @@
+"use strict";
+/* jshint node: true */
+/* globals DENY */
+
+var util = require('util');
+
 exports.register = function() {
     var plugin = this;
 
-    var load_host_list = function () {
-         plugin.loginfo(plugin, "loading host_list");
-         plugin.host_list = plugin.config.get('host_list', 'list', load_host_list);
-     };
-    load_host_list();
-
-    var load_ldap_conf = function() {
-        plugin.loginfo("loading rcpt_to.ldap.ini");
-        plugin.ldap_conf = plugin.config.get('rcpt_to.ldap.ini', 'ini', load_ldap_conf);
+    try {
+        plugin.ldap = require('ldapjs');
     }
-    load_ldap_conf();
+    catch(e) {
+        plugin.logerror("failed to load ldapjs, try installing it (npm install ldapjs)");
+        return;
+    }
+
+    // only load this stuff if ldapjs loaded
+    plugin.load_host_list();
+    plugin.load_ldap_ini();
+    plugin.register_hook('rcpt', 'validate_rcpt');
 };
 
-exports.hook_rcpt = function(next, connection, params) {
+exports.load_ldap_ini = function() {
+    var plugin = this;
+    plugin.loginfo("loading rcpt_to.ldap.ini");
+    plugin.cfg = plugin.config.get('rcpt_to.ldap.ini', 'ini', plugin.load_ldap_ini);
+};
 
-  var ldap = require('ldapjs');
-  var util = require('util');
-  var host_list;
-  var plugin = this;
-  var domain;
+exports.load_host_list = function () {
+    var plugin = this;
+    plugin.loginfo(plugin, "loading host_list");
+    plugin.host_list = plugin.config.get('host_list', 'list', plugin.load_host_list);
+};
 
-  domain = params[0].host.toLowerCase();
-  if (plugin.host_list.indexOf(domain) == -1) {
-    connection.loginfo("Recipient domain is not a local domain; skipping ldap checks.", this);
-    return next();
-  }
+exports.validate_rcpt = function(next, connection, params) {
+    var plugin = this;
 
-  var ar = connection.transaction.results.get('access');
-  if (ar.pass.length >= 1) {
-    if (ar.pass.indexOf("rcpt_to.access.whitelist") > -1) {
-      connection.loginfo("Accepting recipient since its whitelisted already.", this);
-      return next();
+    var domain = params[0].host.toLowerCase();
+    if (plugin.host_list.indexOf(domain) == -1) {
+        connection.loginfo(plugin, "Recipient domain is not local; skipping ldap check.");
+        return next();
     }
-  }
 
-  var client = ldap.createClient({
-    url: plugin.ldap_conf.main.server
-  });
-
-  client.bind(plugin.ldap_conf.main.binddn, plugin.ldap_conf.main.bindpw, function(err) {
-    connection.logerror('error: ' + err, connection);
-  });
-
-  var rcpt = params[0];
-  var plain_rcpt = JSON.stringify(rcpt.original).replace('<', '').replace('>', '').replace('"', '').replace('"', '');
-
-  var opts = {
-    filter: '(&(objectClass=' + plugin.ldap_conf.main.objectclass + ')(|(mail=' + plain_rcpt  + ')(mailAlternateAddress=' + plain_rcpt + ')))',
-    scope: 'sub',
-    attributes: ['dn', 'mail', 'mailAlternateAddress']
-  };
-
-  this.logdebug("Search filter is: " + util.inspect(opts), connection);
-
-  client.search(plugin.ldap_conf.main.basedn, opts, function(err, res) {
-    var items = []
-    res.on('searchEntry', function(entry) {
-        connection.logdebug('entry: ' + JSON.stringify(entry.object), connection);
-        items.push(entry.object);
-      });
-
-    res.on('error', function(err) {
-        connection.logerror('LDAP search error: ' + err, connection);
-      });
-
-    res.on('end', function(result) {
-        connection.logdebug('LDAP search results: ' + items.length + ' -- ' + util.inspect(items));
-
-        if (!items.length) {
-          return next(DENY, "Sorry - no mailbox here by that name.");
-        } else {
-          next();
+    var ar = connection.transaction.results.get('access');
+    if (ar && ar.pass.length >= 1) {
+        if (ar.pass.indexOf("rcpt_to.access.whitelist") !== -1) {
+            connection.loginfo(plugin, "Accepting whitelisted recipient.");
+            return next();
         }
+    }
 
-      });
+    var client = plugin.ldap.createClient({
+        url: plugin.cfg.main.server
+    });
 
-  });
-}
+    client.bind(plugin.cfg.main.binddn, plugin.cfg.main.bindpw, function(err) {
+        connection.logerror(plugin, 'error: ' + err);
+    });
+
+    var rcpt = params[0];
+    var plain_rcpt = JSON.stringify(rcpt.original).replace('<', '').replace('>', '').replace('"', '').replace('"', '');
+
+    var opts = {
+        filter: '(&(objectClass=' + plugin.cfg.main.objectclass + ')(|(mail=' + plain_rcpt  + ')(mailAlternateAddress=' + plain_rcpt + ')))',
+        scope: 'sub',
+        attributes: ['dn', 'mail', 'mailAlternateAddress']
+    };
+
+    connection.logdebug(plugin, "Search filter is: " + util.inspect(opts));
+
+    client.search(plugin.cfg.main.basedn, opts, function(err, res) {
+        var items = [];
+        res.on('searchEntry', function(entry) {
+            connection.logdebug(plugin, 'entry: ' + JSON.stringify(entry.object));
+            items.push(entry.object);
+        });
+
+        res.on('error', function(err) {
+            connection.logerror(plugin, 'LDAP search error: ' + err);
+        });
+
+        res.on('end', function(result) {
+            connection.logdebug(plugin, 'LDAP search results: ' + items.length + ' -- ' + util.inspect(items));
+
+            if (!items.length) {
+                return next(DENY, "Sorry - no mailbox here by that name.");
+            } else {
+                next();
+            }
+        });
+    });
+};
