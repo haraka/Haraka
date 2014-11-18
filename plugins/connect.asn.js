7bb49b74c1cd519b4a927620a22cdb096735bc10
Matt Simerson <matt@tnpi.net>
2014-11-17 22:22:38 -0800

    asn: log some more debugging

--- plugins/connect.asn.js
+++ plugins/connect.asn.js
@@ -57,7 +57,7 @@ exports.get_dns_results = function (zone, ip, cb) {
             plugin.logerror(plugin, "error: " + err + ' running: '+query);
             return cb(zone, null);
         }
-
+console.error(addrs);
         if (!addrs || !addrs[0]) {
             plugin.logerror(plugin, 'no ' + zone + ' results for ' + query);
             return cb(zone, null);
