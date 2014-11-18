9ff7bd894bf3ae780a8deb07270841b01aea2b3b
Matt Simerson <matt@tnpi.net>
2014-11-17 21:38:07 -0800

    dnswl: moved config loading into function w/cb

--- plugins/dnswl.js
+++ plugins/dnswl.js
@@ -1,55 +1,61 @@
+'use strict';
 // dnswl plugin
 
-var cfg;
-
 exports.register = function() {
-    cfg = this.config.get('dnswl.ini');
-    this.inherits('dns_list_base');
+    var plugin = this;
+    plugin.load_dnswl_ini();
+    plugin.inherits('dns_list_base');
 
-    if (cfg.main.enable_stats) {
-        this.logdebug('stats reporting enabled');
-        this.enable_stats = true;
-    }
-
-    if (cfg.main.stats_redis_host) {
-        this.redis_host = cfg.main.stats_redis_host;
-        this.logdebug('set stats redis host to: ' + this.redis_host);
-    }
-
-    this.zones = [];
-    // Compatibility with old-plugin
-    this.zones = this.zones.concat(this.config.get('dnswl.zones', 'list'));
-    if (cfg.main.zones) {
-        this.zones = this.zones.concat(cfg.main.zones.replace(/\s+/g,'').split(/[;,]/));
-    }
-
-    if (cfg.main.periodic_checks) {
-        this.check_zones(cfg.main.periodic_checks);
-    }
-
-    var self = this;
     // IMPORTANT: don't run this on hook_rcpt otherwise we're an open relay...
     ['ehlo','helo','mail'].forEach(function (hook) {
-        self.register_hook(hook, 'check_dnswl');
+        plugin.register_hook(hook, 'check_dnswl');
     });
-}           
-            
-exports.check_dnswl = function (next, connection) {
-    (connection.notes.dnswl) ? next(OK) : next();
-}
+};
 
-exports.hook_connect = function(next, connection) {
-    if (!this.zones || !this.zones.length) {
-        connection.logerror(this, "no zones");
+exports.load_dnswl_ini = function () {
+    var plugin = this;
+    plugin.cfg = plugin.config.get('dnswl.ini', exports.load_dnswl_ini);
+
+    if (plugin.cfg.main.enable_stats) {
+        plugin.logdebug('stats reporting enabled');
+        plugin.enable_stats = true;
+    }
+
+    if (plugin.cfg.main.stats_redis_host) {
+        plugin.redis_host = plugin.cfg.main.stats_redis_host;
+        plugin.logdebug('set stats redis host to: ' + plugin.redis_host);
+    }
+
+    plugin.zones = [];
+    // Compatibility with old-plugin
+    plugin.zones = plugin.zones.concat(
+        plugin.config.get('dnswl.zones', 'list')
+    );
+    if (plugin.cfg.main.zones) {
+        plugin.zones = plugin.zones.concat(
+            plugin.cfg.main.zones.replace(/\s+/g,'').split(/[;,]/));
+    }
+
+    if (plugin.cfg.main.periodic_checks) {
+        plugin.check_zones(plugin.cfg.main.periodic_checks);
+    }
+};
+
+exports.check_dnswl = function (next, connection) {
+    return connection.notes.dnswl ? next(OK) : next();
+};
+
+exports.hook_connect = function (next, connection) {
+    var plugin = this;
+    if (!plugin.zones || !plugin.zones.length) {
+        connection.logerror(plugin, 'no zones');
         return next();
     }
-    var self = this;
-    this.first(connection.remote_ip, this.zones, function (err, zone, a) {
-        if (a) {
-            connection.loginfo(self, connection.remote_ip + ' is whitelisted by ' + zone + ': ' + a);
-            connection.notes.dnswl = true;
-            return next(OK);
-        }
-        return next();
+    plugin.first(connection.remote_ip, plugin.zones, function (err, zone, a) {
+        if (!a) return next();
+        connection.loginfo(plugin, connection.remote_ip +
+            ' is whitelisted by ' + zone + ': ' + a);
+        connection.notes.dnswl = true;
+        return next(OK);
     });
-}
+};
