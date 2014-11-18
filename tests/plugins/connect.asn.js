482684ac32b5a144e0fe2be8a909197d2863d4dc
Matt Simerson <matt@tnpi.net>
2014-11-17 21:49:05 -0800

    update two remaining tests with 'fix'

--- tests/plugins/connect.asn.js
+++ tests/plugins/connect.asn.js
@@ -7,7 +7,7 @@ function _set_up(callback) {
     this.backup = {};
 
     // needed for tests
-    this.plugin = Plugin('connect.asn');
+    this.plugin = new Plugin('connect.asn');
     this.plugin.cfg = { main: {} };
     this.connection = Connection.createConnection();
 
@@ -24,16 +24,20 @@ exports.parse_monkey = {
     '15169/23': function (test) {
         test.expect(1);
         test.deepEqual(
-                {net: '74.125.44.0/23', asn: '15169', org: 'Google Inc.', date: '2000-03-30', country: undefined},
-                this.plugin.parse_monkey('74.125.44.0/23 | AS15169 | Google Inc. | 2000-03-30')
+                {   net: '74.125.44.0/23', asn: '15169', org: 'Google Inc.',
+                    date: '2000-03-30', country: undefined},
+                this.plugin.parse_monkey(
+                    '74.125.44.0/23 | AS15169 | Google Inc. | 2000-03-30')
                 );
         test.done();
     },
     '15169/16': function (test) {
         test.expect(1);
         test.deepEqual(
-                {net: '74.125.0.0/16', asn: '15169', org: 'Google Inc.', date: '2000-03-30', country: 'US'},
-                this.plugin.parse_monkey('74.125.0.0/16 | AS15169 | Google Inc. | 2000-03-30 | US')
+                {   net: '74.125.0.0/16', asn: '15169', org: 'Google Inc.',
+                    date: '2000-03-30', country: 'US'},
+                this.plugin.parse_monkey(
+                    '74.125.0.0/16 | AS15169 | Google Inc. | 2000-03-30 | US')
                 );
         test.done();
     },
@@ -76,16 +80,20 @@ exports.parse_cymru = {
     '40431': function (test) {
         test.expect(1);
         test.deepEqual(
-                { asn: '40431', net: '208.75.176.0/21', country: 'US', assignor: 'arin', date: '2007-03-02' },
-                this.plugin.parse_cymru('40431 | 208.75.176.0/21 | US | arin | 2007-03-02')
+                {   asn: '40431', net: '208.75.176.0/21', country: 'US',
+                    assignor: 'arin', date: '2007-03-02' },
+                this.plugin.parse_cymru(
+                    '40431 | 208.75.176.0/21 | US | arin | 2007-03-02')
                 );
         test.done();
     },
     '10290': function (test) {
         test.expect(1);
         test.deepEqual(
-                { asn: '10290', net: '12.129.48.0/24', country: 'US', assignor: 'arin', date: ''},
-                this.plugin.parse_cymru('10290 | 12.129.48.0/24 | US | arin |')
+                {   asn: '10290', net: '12.129.48.0/24', country: 'US',
+                    assignor: 'arin', date: ''},
+                this.plugin.parse_cymru(
+                    '10290 | 12.129.48.0/24 | US | arin |')
                 );
         test.done();
     },
@@ -103,10 +111,10 @@ exports.get_dns_results = {
             test.equal('8.8.8.0/24', obj.net);
             test.done();
         };
-        this.plugin.get_dns_results('origin.asn.cymru.com', '8.8.8.8', cb)
+        this.plugin.get_dns_results('origin.asn.cymru.com', '8.8.8.8', cb);
     },
     'asn.routeviews.org': function (test) {
-        test.expect(3);
+        test.expect(2);
         var cb = function (zone, obj) {
             test.equal('asn.routeviews.org', zone);
             if (obj.asn && obj.asn === '15169') {
@@ -118,9 +126,7 @@ exports.get_dns_results = {
             }
             test.done();
         };
-        test.ok(
-                this.plugin.get_dns_results('asn.routeviews.org', '8.8.8.8', cb)
-            );
+        this.plugin.get_dns_results('asn.routeviews.org', '8.8.8.8', cb);
     },
     'origin.asn.spameatingmonkey.net': function (test) {
         var cb = function (zone, obj) {
@@ -129,9 +135,11 @@ exports.get_dns_results = {
             test.equal('US', obj.country);
             test.done();
         };
-        test.expect(4);
-        test.ok(
-                this.plugin.get_dns_results('origin.asn.spameatingmonkey.net', '8.8.8.8', cb)
-            );
+        test.expect(3);
+        this.plugin.get_dns_results(
+                'origin.asn.spameatingmonkey.net',
+                '8.8.8.8',
+                cb
+                );
     },
 };
