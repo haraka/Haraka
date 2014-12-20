3fb9e8052530c185137a4563bf1e6517bde343d9
Matt Simerson <matt@tnpi.net>
2014-12-19 22:10:02 -0800

    tis: add basic tests, don't register unless certs
    * fixes a crasher bug (missing config_options in load_config)
    * added test coverage for register() and set up
    * don’t register hooks unless certs are present

--- plugins/tls.js
+++ plugins/tls.js
@@ -10,27 +10,27 @@ var utils = require('./utils');
 exports.register = function () {
     var plugin = this;
 
+    // declare first, these opts might be updated by tls.ini
     plugin.tls_opts = {
-        key: false,
-        cert: false,
+        key: plugin.load_pem('tls_key.pem'),
+        cert: plugin.load_pem('tls_cert.pem'),
     };
 
-    var config_options = ['ciphers','requestCert','rejectUnauthorized'];
-
-    plugin.load_config();
-
-    plugin.tls_opts.key = plugin.load_pem('tls_key.pem');
-    if (!plugin.tls_opts.key) {
-        plugin.logcrit("config/tls_key.pem not loaded. See 'haraka -h tls'");
-    }
-
-    plugin.tls_opts.cert = plugin.load_pem('tls_cert.pem');
-
-    if (!plugin.tls_opts.cert) {
-        plugin.logcrit("config/tls_cert.pem not loaded. See 'haraka -h tls'");
-    }
+    plugin.load_tls_ini();
 
     plugin.logdebug(plugin.tls_opts);
+
+    if (!plugin.tls_opts.key) {
+        plugin.logcrit("config/tls_key.pem not loaded. See 'haraka -h tls'");
+        return;
+    }
+    if (!plugin.tls_opts.cert) {
+        plugin.logcrit("config/tls_cert.pem not loaded. See 'haraka -h tls'");
+        return;
+    }
+    
+    plugin.register_hook('capabilities', 'tls_capabilities');
+    plugin.register_hook('unrecognized_command', 'tls_unrecognized_command');
 };
 
 exports.load_pem = function (file) {
@@ -38,7 +38,7 @@ exports.load_pem = function (file) {
     return plugin.config.get(file, 'binary');
 };
 
-exports.load_config = function () {
+exports.load_tls_ini = function () {
     var plugin = this;
     plugin.cfg = plugin.config.get('tls.ini', {
         booleans: [
@@ -46,33 +46,29 @@ exports.load_config = function () {
             '-main.rejectUnauthorized',
         ]
     }, function () {
-        plugin.load_config();
+        plugin.load_tls_ini();
     });
 
-    for (var i in config_options) {
-        if (plugin.cfg.main[config_options[i]] === undefined) { continue; }
-        plugin.tls_opts[config_options[i]] = plugin.cfg.main[config_options[i]];
+    if (!plugin.cfg.no_tls_hosts) {
+        plugin.cfg.no_tls_hosts = {};
+    }
+
+    var config_options = ['ciphers','requestCert','rejectUnauthorized'];
+
+    for (var i = 0; i < config_options.length; i++) {
+        var opt = config_options[i];
+        if (plugin.cfg.main[opt] === undefined) { continue; }
+        plugin.tls_opts[opt] = plugin.cfg.main[opt];
     }
 };
 
-exports.hook_capabilities = function (next, connection) {
-    /* Caution: do not advertise STARTTLS if the upgrade has already been done. */
+exports.tls_capabilities = function (next, connection) {
+    /* Caution: do not advertise STARTTLS if already TLS upgraded */
     if (connection.using_tls) { return next(); }
 
     var plugin = this;
-    if (plugin.cfg.no_tls_hosts) {
-        if (plugin.cfg.no_tls_hosts[connection.remote_ip]) {
-            return next();
-        }
-    }
-
-    if (!plugin.tls_opts.key) {
-        connection.logcrit("No TLS key found. See 'harka -h tls'");
-        return next();
-    }
-
-    if (!plugin.tls_opts.cert) {
-        connection.logcrit("No TLS cert found. See 'harka -h tls'");
+    
+    if (plugin.cfg.no_tls_hosts[connection.remote_ip]) {
         return next();
     }
 
@@ -83,7 +79,7 @@ exports.hook_capabilities = function (next, connection) {
     next();
 };
 
-exports.hook_unrecognized_command = function (next, connection, params) {
+exports.tls_unrecognized_command = function (next, connection, params) {
     /* Watch for STARTTLS directive from client. */
     if (!connection.notes.tls_enabled) { return next(); }
     if (params[0].toUpperCase() !== 'STARTTLS') { return next(); }
