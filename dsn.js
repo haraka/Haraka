1d509ff18671ac35e10b39e5d0ac454321fe401c
Steve Freegard <steve.freegard@fsl.com>
2015-05-28 12:07:13 +0000

    Export DSN constructor and allow custom codes

--- dsn.js
+++ dsn.js
@@ -72,9 +72,9 @@ function DSN(code, msg, def, subject, detail) {
     this.code = (/^[245]\d{2}/.exec(code)) ? code : null || def || 450;
     this.msg = msg;
     this.cls = parseInt(new String(this.code)[0]);
-    this.sub = (enum_status_codes[subject]) ? subject : 0;
-    this.det = (enum_status_codes[this.sub][detail]) ? detail : 0;
-    this.default_msg = enum_status_codes[this.sub][this.det];
+    this.sub = subject || 0;
+    this.det = detail || 0;
+    this.default_msg = ((enum_status_codes[this.sub]) ? enum_status_codes[this.sub][this.det] : '') || '';
     // Handle multi-line replies
     if (Array.isArray(this.msg)) {
         this.reply = [];
@@ -86,6 +86,8 @@ function DSN(code, msg, def, subject, detail) {
         this.reply = [this.cls, this.sub, this.det].join('.') + ' ' + (this.msg || this.default_msg);
     }
 }
+
+exports.DSN = DSN;
 
 exports.unspecified                 = function(msg, code) { return new DSN(code, msg, 450, 0, 0); }
 
