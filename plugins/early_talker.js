810f9a2824056ae782f3c97cc0e0740df5ffc4e5
Steve Freegard <steve.freegard@fsl.com>
2015-07-06 21:58:06 +0000

    Fix early_talker to work at connect_init as well as data

--- plugins/early_talker.js
+++ plugins/early_talker.js
@@ -2,9 +2,8 @@
 
 exports.register = function() {
     var plugin = this;
-
     plugin.load_config();
-
+    plugin.register_hook('connect_init', 'early_talker');
     plugin.register_hook('data', 'early_talker');
 };
 
@@ -18,8 +17,9 @@ exports.load_config = function () {
 
 exports.early_talker = function(next, connection) {
     var plugin = this;
-    if (!plugin.pause      ) { return next(); } // config set to 0
-    if (connection.relaying) {   // Don't pause AUTH/RELAY clients
+    if (!plugin.pause) return next();
+    if (connection.relaying) {
+        // Don't delay AUTH/RELAY clients
         if (connection.early_talker) {
             connection.results.add(plugin,
                     { skip: 'an early talking relaying client?!'});
@@ -27,8 +27,19 @@ exports.early_talker = function(next, connection) {
         return next();
     }
 
-    setTimeout(function () {
-        if (!connection.early_talker) { return next(); }
-        next(DENYDISCONNECT, "You talk too soon");
-    }, plugin.pause);
+    var check = function () {
+        if (!connection.early_talker) return next();
+        return next(DENYDISCONNECT, "You talk too soon");
+    }
+
+    var elapsed = (Date.now() - connection.start_time);
+    if (elapsed > plugin.pause && plugin.hook === 'connect_init') {
+        // Something else already waited
+        return check();
+    }
+    else {
+        setTimeout(function () {
+            return check();
+        }, plugin.pause);
+    }
 };
