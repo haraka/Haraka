8200c1c02bc18fe5f6c42261756f337b0c035105
Matt Simerson <matt@tnpi.net>
2015-07-11 22:43:28 -0700

    early: added reject option, save pass/fail results
    * move config into early_talker.ini
    * with fallback to early_talker.pause
    * added reject option (default: true)
    * save pass/fail results
    * only calculate elapsed on hook=connect_init
    * when pausing, subtract elapsed from pause
    * make sure connection exists after delay

--- docs/plugins/early_talker.md
+++ docs/plugins/early_talker.md
@@ -1,14 +1,11 @@
 early\_talker
 ============
 
-Early talkers are violators of the SMTP specification, which demands that
+Early talkers are violators of the SMTP specification, which require that
 clients must wait for certain responses before sending the next command.
 
-Early talker detection is handled internally by Haraka (in connection.js).
-
 This plugin introduces a configurable delay before the connection banner
-is sent and after the DATA command is sent to allow the client to send 
-data and for Haraka to detect if it talks early.
+and after the DATA command for Haraka to detect if it talks early.
 
 If an early talker is detected at connection or DATA, then a DENY is
 returned with the message 'You talk too soon'.
@@ -16,7 +13,8 @@ returned with the message 'You talk too soon'.
 Configuration
 -------------
 
-* early\_talker.pause
+The config file early\_talker.ini has two options:
 
-  Specifies a delay in milliseconds to delay before each SMTP command before
-  sending the response, while waiting for early talkers. Default is no pause.
+- pause: the delay in seconds before each SMTP command. Default is no pause.
+
+- reject: whether or not to reject for early talkers. Default is true;
--- plugins/early_talker.js
+++ plugins/early_talker.js
@@ -4,11 +4,26 @@ exports.register = function() {
     var plugin = this;
     plugin.load_config();
     plugin.register_hook('connect_init', 'early_talker');
-    plugin.register_hook('data', 'early_talker');
+    plugin.register_hook('data',         'early_talker');
 };
 
 exports.load_config = function () {
     var plugin = this;
+
+    plugin.cfg = plugin.config.get('early_talker.ini', {
+        booleans: [
+            '+main.reject',
+            ]
+    },
+    function () {
+        plugin.load_config();
+    });
+
+    if (plugin.cfg.main && plugin.cfg.main.pause) {
+        plugin.pause = plugin.cfg.main.pause * 1000;
+        return;
+    }
+
     // config/early_talker.pause is in milliseconds
     plugin.pause = plugin.config.get('early_talker.pause', function () {
         plugin.load_config();
@@ -18,28 +33,34 @@ exports.load_config = function () {
 exports.early_talker = function(next, connection) {
     var plugin = this;
     if (!plugin.pause) return next();
-    if (connection.relaying) {
-        // Don't delay AUTH/RELAY clients
+
+    if (connection.relaying) {    // Don't delay AUTH/RELAY clients
         if (connection.early_talker) {
-            connection.results.add(plugin,
-                    { skip: 'an early talking relaying client?!'});
+            connection.results.add(plugin, { skip: 'relay client'});
         }
         return next();
     }
 
     var check = function () {
-        if (!connection.early_talker) return next();
+        if (!connection) return next();
+        if (!connection.early_talker) {
+            connection.results.add(plugin, {pass: 'early'});
+            return next();
+        }
+        connection.results.add(plugin, {fail: 'early'});
+        if (!plugin.cfg.main.reject) return next();
         return next(DENYDISCONNECT, "You talk too soon");
+    };
+
+    var pause = plugin.pause;
+    if (plugin.hook === 'connect_init') {
+        var elapsed = (Date.now() - connection.start_time);
+        if (elapsed > plugin.pause) {
+            // Something else already waited
+            return check();
+        }
+        pause = plugin.pause - elapsed;
     }
 
-    var elapsed = (Date.now() - connection.start_time);
-    if (elapsed > plugin.pause && plugin.hook === 'connect_init') {
-        // Something else already waited
-        return check();
-    }
-    else {
-        setTimeout(function () {
-            return check();
-        }, plugin.pause);
-    }
+    setTimeout(function () { check(); }, pause);
 };
