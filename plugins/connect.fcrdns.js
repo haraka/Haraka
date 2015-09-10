7c2ed87d8be620869f266d12aee6d4b9481bfacb
Matt Simerson <matt@tnpi.net>
2015-09-10 12:24:01 -0700

    repair ptr_valid errors introduced with #1123

--- plugins/connect.fcrdns.js
+++ plugins/connect.fcrdns.js
@@ -71,7 +71,7 @@ exports.hook_lookup_rdns = function (next, connection) {
         connection.results.add(plugin, {ptr_names: ptr_names});
         connection.results.add(plugin, {has_rdns: true});
 
-        // Fetch A records for each PTR host name
+        // Fetch A & AAAA records for each PTR host name
         var pending_queries = 0;
         var queries_run = false;
         var results = {};
@@ -93,37 +93,42 @@ exports.hook_lookup_rdns = function (next, connection) {
             pending_queries++;
             (function (ptr_domain) {  /* BEGIN BLOCK SCOPE */
                 async.parallel([
-                    function(callback){
-                        dns.resolve4(ptr_domain, function(err, ips_from_fwd) {
+                    function (callback) {
+                        dns.resolve4(ptr_domain, function (err, ips_from_fwd) {
                             if (err) {
-                                plugin.handle_a_error(connection, err, ptr_domain);
+                                connection.results.add(plugin, {err: err.message});
                             }
-                            callback(err, ips_from_fwd);
+                            // the host might be IPv6 only, ignore error
+                            callback(null, ips_from_fwd);
                         });
                     },
-                    function(callback){
-                        dns.resolve6(ptr_domain, function(err, ips_from_fwd) {
+                    function (callback) {
+                        dns.resolve6(ptr_domain, function (err, ips_from_fwd) {
                             if (err) {
-                                plugin.handle_a_error(connection, err, ptr_domain);
+                                connection.results.add(plugin, {err: err.message});
                             }
-                            callback(err, ips_from_fwd);
+                            // not having a AAAA isn't an error...yet
+                            callback(null, ips_from_fwd);
                         });
                     }
                 ],
-                function(err, async_results) {
+                function (err, async_results) {
                     pending_queries--;
                     var ips = [];
-                    // results is now equals to: {queryA: 1, queryAAAA: 2}
+                    // results are: { queryA: 1, queryAAAA: 2 }
                     for (var i=0; i<async_results.length; i++) {
-                        if(async_results[i]){
-                            ips = ips.concat(async_results[i]);
-                        }
+                        if (!async_results[i]) continue;
+                        ips = ips.concat(async_results[i]);
                     }
 
                     connection.logdebug(plugin, ptr_domain + ' => ' + ips);
                     results[ptr_domain] = ips;
 
                     if (pending_queries > 0) return;
+
+                    if (ips.length === 0) {
+                        connection.results.add(plugin, {fail: 'ptr_valid('+ptr_domain+')' });
+                    }
 
                     // Got all DNS results
                     connection.results.add(plugin, {ptr_name_to_ip: results});
@@ -165,23 +170,7 @@ exports.hook_data_post = function (next, connection) {
     return next();
 };
 
-exports.handle_a_error = function(connection, err, domain) {
-    var plugin = this;
-
-    switch (err.code) {
-        case 'ENOTFOUND':
-        case 'NXDOMAIN':
-        case 'ENODATA':
-        case dns.NOTFOUND:
-        case dns.NXDOMAIN:
-            connection.results.add(plugin, {fail: 'ptr_valid('+domain+')' });
-            break;
-        default:
-            connection.results.add(plugin, {err: err.message});
-    }
-};
-
-exports.handle_ptr_error = function(connection, err, do_next) {
+exports.handle_ptr_error = function (connection, err, do_next) {
     var plugin = this;
     var rip = connection.remote_ip;
 
@@ -203,7 +192,7 @@ exports.handle_ptr_error = function(connection, err, do_next) {
     }
 };
 
-exports.check_fcrdns = function(connection, results, do_next) {
+exports.check_fcrdns = function (connection, results, do_next) {
     var plugin = this;
 
     for (var fdom in results) {    // mail.example.com
