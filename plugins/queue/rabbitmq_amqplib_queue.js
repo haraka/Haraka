43495bd1de5011fca0b40c23242a1dd24b123c96
Joe Vanderstelt <thisboyiscrazy@gmail.com>
2016-01-25 11:02:37 -0500

    code fixes and documentation

--- plugins/queue/rabbitmq_amqplib_queue.js
+++ plugins/queue/rabbitmq_amqplib_queue.js
@@ -6,48 +6,64 @@ var channel;
 var deliveryMode;
 var logger = require('./logger');
 var queueRCPTReplace;
+var durable;
+var autoDelete;
+
+var async = require('async');
 
 exports.register = function () {
     this.init_amqp_connection();
+    //this.register_hook('queue','hook_queue');
 };
 
-exports.hook_queue = function(next, connection) {
+exports.hook_data = function (next, connection) {
+    connection.transaction.parse_body = 1;
+    next();
+}
 
-    var queues;
-    var rctp_to = connection.trasaction.rcpt_to.length;
+exports.hook_queue_outbound = function(next, connection) {
+
+    var queues = {};
+    var rcpt_to = connection.transaction.rcpt_to;
     var lq = queueRCPTReplace.length;
     var lr = rcpt_to.length;
     for (var i = 0; i < lq; i++) {
         for (var j = 0; j <lr; j++) {
-            if (rctp_to[j].match(queueRCPTReplace[i][0])) {
-                queues[rctp_to[j].replace(queueRCPTReplace[i][0],queueRCPTReplace[i][1])] = true;
+            if (rcpt_to[j].original.match(queueRCPTReplace[i][0])) {
+                queues[rcpt_to[j].original.replace(queueRCPTReplace[i][0],queueRCPTReplace[i][1])] = true;
             };
         };
     };
 
-    var sent = false;
-
     var send_message = function(queue) {
-        connection.transaction.message_stream.get_data(function(str) {
-            sent = sent && (channel.sendToQueue(queue, new Buffer(str), {deliveryMode: deliveryMode}))
-        });
+        var message = JSON.stringify(connection.transaction.body);
+        message = new Buffer(message);
+        return channel.sendToQueue(queue, message, {deliveryMode: deliveryMode});
     };
 
-    queues = queues.keys();
-    var l = queues.length;
-    for (var i = 0; i < l; i++) {
+    async.forEachOf(queues,function(item,key,callback) {
         channel.assertQueue(
-	    queueName,
+	    key,
             {durable: durable, autoDelete: autoDelete},
-	    function (err, ok) {if (!err) send_message(ok.queue);}
+	    function (err, ok) {
+	        if (err) {
+		    callback(err);
+		} else {
+		    if (send_message(ok.queue)) {
+		        callback();
+                    } else {
+                        callback("RabbitMQ Queue Falied");
+                   };
+		}
+	    }
 	);
-    };
-
-    if (sent) {
-        return next(OK);
-    } else {
-        return next();
-    };
+    },function(err) {
+        if (err) {
+            next();
+        } else {
+            next(OK);
+        };
+    });
 };
 
 exports.init_amqp_connection = function() {
@@ -59,13 +75,14 @@ exports.init_amqp_connection = function() {
     var password = cfg.password || "guest";
     var exchangeName = cfg.exchangeName || "emailMessages";
     var exchangeType = cfg.exchangeType || "direct";
-    var durable = cfg.durable === "true" || true;
     // var confirm = cfg.confirm === "true" || true;
-    var autoDelete = cfg.autoDelete === "true" || false;
+
+    autoDelete = cfg.autoDelete === "true" || false;
+    durable = cfg.durable === "true" || true;
     queueRCPTReplace = cfg.queueRCPTReplace || [[".*","emails"]];
     var l = queueRCPTReplace.length;
     for (var i = 0; i < l; i ++) {
-        queueRCPTReplace[i][0] = new Regexp("^" +  queueRCPTReplace[i][0] + "$");
+        queueRCPTReplace[i][0] = new RegExp("^" +  queueRCPTReplace[i][0] + "$");
     };
 
     deliveryMode = cfg.deliveryMode || 2;
