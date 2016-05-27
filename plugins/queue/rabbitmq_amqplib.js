ff0bf0ed43371a497cb42675562e76ef217eb260
Matt Sergeant <matt@sergeant.org>
2016-05-26 20:38:41 -0400

    Belt and braces for amqplib plugin

--- plugins/queue/rabbitmq_amqplib.js
+++ plugins/queue/rabbitmq_amqplib.js
@@ -10,16 +10,21 @@ exports.register = function () {
     this.init_amqp_connection();
 }
 
-exports.hook_queue = function(next, connection) {
+exports.rabbitmq_queue = function(next, connection) {
+    var plugin = this;
     connection.transaction.message_stream.get_data(function(str) {
-        if (channel.sendToQueue(queue, new Buffer(str), {deliveryMode: deliveryMode}))
+        if (channel && channel.sendToQueue(queue, new Buffer(str), {deliveryMode: deliveryMode})) {
             return next(OK);
-        else
+        }
+        else {
+            plugin.logerror("Failed to queue to rabbitmq");
             return next();
+        }
     });
 };
 
 exports.init_amqp_connection = function() {
+    var plugin = this;
     var cfg = this.config.get("rabbitmq.ini").rabbitmq;
 
     var host = cfg.host || "127.0.0.1";
@@ -35,19 +40,31 @@ exports.init_amqp_connection = function() {
     deliveryMode = cfg.deliveryMode || 2;
 
     amqp.connect("amqp://"+user+":"+password+"@"+host+":"+port, function(err, conn){
-        if (err)
-            return conn.close();
+        if (err) {
+            plugin.logerror("Connection to rabbitmq failed: " + err);
+            return;
+        }
         // TODO: if !confirm conn.createChannel...
         conn.createConfirmChannel(function (err2, ch) {
-            if (err2) return conn.close();
+            if (err2) {
+                plugin.logerror("Error creating rabbitmq channel: " + err2);
+                return conn.close();
+            }
             ch.assertExchange(exchangeName, exchangeType, {durable: durable}, function(err3, ok){
-                if (err3) return conn.close();
+                if (err3) {
+                    plugin.logerror("Error asserting rabbitmq exchange: " + err3);
+                    return conn.close();
+                }
                 ch.assertQueue(queueName,
                     {durable: durable, autoDelete: autoDelete},
                     function (err4, ok2) {
-                        if (err4) return conn.close();
+                        if (err4) {
+                            plugin.logerror("Error asserting rabbitmq queue: " + err4);
+                            return conn.close();
+                        }
                         queue = ok2.queue;
                         channel = ch;
+                        plugin.register_hook('queue', 'rabbitmq_queue');
                     }
                 );
             });
