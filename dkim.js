7855fc4b9ec139ac61fd0272ed1387c595aee94e
Rachel Chen <me@rachelchen.me>
2016-06-07 17:05:20 -0700

    DKIMObject needs to have a copy of header_idx

--- dkim.js
+++ dkim.js
@@ -41,6 +41,9 @@ function Buf() {
     };
 }
 
+function cloneHack(object) {
+    return JSON.parse(JSON.stringify(object));
+}
 
 ////////////////
 // DKIMObject //
@@ -53,7 +56,7 @@ function DKIMObject (header, header_idx, cb, timeout) {
     this.sig = header;
     this.sig_md5 = md5(header);
     this.run_cb = false;
-    this.header_idx = header_idx;
+    this.header_idx = cloneHack(header_idx);
     this.timeout = timeout;
     this.fields = {};
     this.headercanon = this.bodycanon = 'simple';
