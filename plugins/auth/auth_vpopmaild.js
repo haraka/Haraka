f00e402061ad39a525e33dfa959db37b12f9f270
Matt Simerson <matt@tnpi.net>
2016-11-07 22:41:40 -0800

    do not toString() when undefined
    avoids a CRIT error

--- plugins/auth/auth_vpopmaild.js
+++ plugins/auth/auth_vpopmaild.js
@@ -155,6 +155,6 @@ exports.get_plain_passwd = function (user, connection, cb) {
         }
     });
     socket.on('end', function () {
-        cb(plain_pass.toString());
+        cb(plain_pass ? plain_pass.toString() : plain_pass);
     });
 };
