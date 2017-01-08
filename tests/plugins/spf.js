haraka/Haraka

commit 2d3ca084de7fed15312734fc3902e817f920d8d3

spf: context=myself fixes (#1528)
* spf: context=myself fixes

* Lint fixes

plugins/spf.js
@@ -123,13 +123,14 @@ exports.hook_mail = function (next, connection, params) {
     var mfrom = params[0].address();
     var host = params[0].host;
     var spf = new SPF();
+    var auth_result;
 
     if (connection.notes.spf_helo) {
         var h_result = connection.notes.spf_helo;
         var h_host = connection.hello_host;
         plugin.save_to_header(connection, spf, h_result, mfrom, h_host, 'helo');
         if (!host) {   // Use results from HELO if the return-path is null
-            var auth_result = spf.result(h_result).toLowerCase();
+            auth_result = spf.result(h_result).toLowerCase();
             connection.auth_results( "spf="+auth_result+" smtp.helo=" + h_host);
 
             var sender = '<> via ' + h_host;
@@ -149,15 +150,17 @@ exports.hook_mail = function (next, connection, params) {
 
     spf.helo = connection.hello_host;
 
-    var ch_cb = function (err, result) {
+    var ch_cb = function (err, result, ip) {
         if (timer) clearTimeout(timer);
         if (timeout) return;
         if (err) {
             connection.logerror(plugin, err);
             return next();
         }
-        plugin.log_result(connection, 'mfrom', host, mfrom, spf.result(result));
-        plugin.save_to_header(connection, spf, result, mfrom, host, 'mailfrom');
+        plugin.log_result(connection, 'mfrom', host, mfrom,
+                          spf.result(result), (ip ? ip : connection.remote_ip));
+        plugin.save_to_header(connection, spf, result, mfrom, host,
+                              'mailfrom', (ip ? ip : connection.remote_ip));
 
         auth_result = spf.result(result).toLowerCase();
         connection.auth_results( "spf="+auth_result+" smtp.mailfrom="+host);
@@ -186,20 +189,37 @@ exports.hook_mail = function (next, connection, params) {
 
     // outbound (relaying), context=myself
     net_utils.get_public_ip(function(e, my_public_ip) {
-        if (e) {
-            return ch_cb(e);
-        }
-        if (!my_public_ip) {
-            return ch_cb(new Error("failed to discover public IP"));
-        }
-        return spf.check_host(my_public_ip, host, mfrom, ch_cb);
+        // We always check the client IP first, because a relay
+        // could be sending inbound mail from a non-local domain
+        // which could case an incorrect SPF Fail result if we
+        // check the public IP first, so we only check the public
+        // IP if the client IP returns a result other than 'Pass'.
+        spf.check_host(connection.remote_ip, host, mfrom, function (err, result) {
+            var spf_result;
+            if (result) {
+                spf_result = spf.result(result).toLowerCase();
+            }
+            if (err || spf_result && spf_result !== 'pass') {
+                if (e) {
+                    // Error looking up public IP
+                    return ch_cb(e);
+                }
+                if (!my_public_ip) {
+                    return ch_cb(new Error("failed to discover public IP"));
+                }
+                return spf.check_host(my_public_ip, host, mfrom, function (er, r) {
+                    return ch_cb(er, r, my_public_ip);
+                });
+            }
+            ch_cb(err, result, connection.remote_ip);
+        });
     });
 };
 
-exports.log_result = function (connection, scope, host, mfrom, result) {
+exports.log_result = function (connection, scope, host, mfrom, result, ip) {
     connection.loginfo(this, [
         'identity=' + scope,
-        'ip=' + connection.remote_ip,
+        'ip=' + (ip ? ip : connection.remote_ip),
         'domain="' + host + '"',
         'mfrom=<' + mfrom + '>',
         'result=' + result
@@ -244,7 +264,7 @@ exports.return_results = function(next, connection, spf, scope, result, sender) 
     }
 };
 
-exports.save_to_header = function (connection, spf, result, mfrom, host, id) {
+exports.save_to_header = function (connection, spf, result, mfrom, host, id, ip) {
     var plugin = this;
     // Add a trace header
     if (!connection) return;
@@ -256,7 +276,7 @@ exports.save_to_header = function (connection, spf, result, mfrom, host, id) {
         connection.remote_ip + ' as permitted sender) ' + [
             'receiver=' + plugin.config.get('me'),
             'identity=' + id,
-            'client-ip=' + connection.remote_ip,
+            'client-ip=' + (ip ? ip : connection.remote_ip),
             'helo=' + connection.hello_host,
             'envelope-from=<' + mfrom + '>'
         ].join('; ')

commit 9313192df92c70839f1b89d9dbb081abf12b664b

spf fix for relaying & context=myself
which has been broken since #1528

the money shot is the line: spf = new SPF();

plugins/spf.js
@@ -16,6 +16,8 @@ exports.register = function () {
 
 exports.load_config = function () {
     var plugin = this;
+    plugin.nu = net_utils;   // so tests can set public_ip
+
     plugin.cfg = plugin.config.get('spf.ini', {
         booleans: [
             '-defer.helo_temperror',
@@ -172,8 +174,7 @@ exports.hook_mail = function (next, connection, params) {
             domain: host,
             emit: true,
         });
-        return plugin.return_results(next, connection, spf, 'mfrom', result,
-            '<'+mfrom+'>');
+        plugin.return_results(next, connection, spf, 'mfrom', result, '<'+mfrom+'>');
     };
 
     // typical inbound (!relay)
@@ -198,7 +199,7 @@ exports.hook_mail = function (next, connection, params) {
             if (result) {
                 spf_result = spf.result(result).toLowerCase();
             }
-            if (err || spf_result && spf_result !== 'pass') {
+            if (err || (spf_result && spf_result !== 'pass')) {
                 if (e) {
                     // Error looking up public IP
                     return ch_cb(e);
@@ -206,9 +207,11 @@ exports.hook_mail = function (next, connection, params) {
                 if (!my_public_ip) {
                     return ch_cb(new Error("failed to discover public IP"));
                 }
-                return spf.check_host(my_public_ip, host, mfrom, function (er, r) {
-                    return ch_cb(er, r, my_public_ip);
+                spf = new SPF();
+                spf.check_host(my_public_ip, host, mfrom, function (er, r) {
+                    ch_cb(er, r, my_public_ip);
                 });
+                return;
             }
             ch_cb(err, result, connection.remote.ip);
         });

tests/plugins/spf.js
@@ -1,11 +1,7 @@
 'use strict';
 
-var path         = require('path');
-
 var Address      = require('address-rfc2821').Address;
 var fixtures     = require('haraka-test-fixtures');
-
-var Connection   = fixtures.connection;
 
 var SPF          = require('../../spf').SPF;
 var spf          = new SPF();
@@ -13,10 +9,12 @@ var spf          = new SPF();
 var _set_up = function (done) {
 
     this.plugin = new fixtures.plugin('spf');
-    this.plugin.config.root_path = path.resolve(__dirname, '../../config');
-    this.plugin.cfg = { main: { }, defer: {}, deny: {} };
+    this.plugin.load_config();
+    this.plugin.timeout = 8000;
 
-    this.connection = Connection.createConnection();
+    this.connection = fixtures.connection.createConnection();
+    this.connection.transaction = fixtures.transaction.createTransaction();
+    this.connection.transaction.results = new fixtures.result_store(this.connection);
 
     done();
 };
@@ -167,6 +165,8 @@ exports.hook_helo = {
 
 };
 
+var test_addr = new Address('<test@example.com>');
+
 exports.hook_mail = {
     setUp : _set_up,
     'rfc1918': function (test) {
@@ -175,8 +175,9 @@ exports.hook_mail = {
             test.done();
         };
         test.expect(1);
+        this.connection.remote.is_private=true;
         this.connection.remote.ip='192.168.1.1';
-        this.plugin.hook_mail(next, this.connection);
+        this.plugin.hook_mail(next, this.connection, [test_addr]);
     },
     'rfc1918 relaying': function (test) {
         var next = function () {
@@ -184,9 +185,10 @@ exports.hook_mail = {
             test.done();
         };
         test.expect(1);
+        this.connection.remote.is_private=true;
         this.connection.remote.ip='192.168.1.1';
         this.connection.relaying=true;
-        this.plugin.hook_mail(next, this.connection);
+        this.plugin.hook_mail(next, this.connection, [test_addr]);
     },
     'no txn': function (test) {
         var next = function () {
@@ -195,6 +197,7 @@ exports.hook_mail = {
         };
         test.expect(1);
         this.connection.remote.ip='207.85.1.1';
+        delete this.connection.transaction;
         this.plugin.hook_mail(next, this.connection);
     },
     'txn, no helo': function (test) {
@@ -203,9 +206,9 @@ exports.hook_mail = {
             test.done();
         };
         test.expect(1);
+        this.plugin.cfg.deny.mfrom_fail = false;
         this.connection.remote.ip='207.85.1.1';
-        this.plugin.hook_mail(next, this.connection,
-            [new Address('<test@example.com>')]);
+        this.plugin.hook_mail(next, this.connection, [test_addr]);
     },
     'txn': function (test) {
         var next = function (rc) {
@@ -215,8 +218,7 @@ exports.hook_mail = {
         test.expect(1);
         this.connection.set('remote', 'ip', '207.85.1.1');
         this.connection.set('hello', 'host', 'mail.example.com');
-        this.plugin.hook_mail(next, this.connection,
-            [new Address('<test@example.com>')]);
+        this.plugin.hook_mail(next, this.connection, [test_addr]);
     },
     'txn, relaying': function (test) {
         var next = function (rc) {
@@ -227,8 +229,23 @@ exports.hook_mail = {
         this.connection.set('remote', 'ip', '207.85.1.1');
         this.connection.relaying=true;
         this.connection.set('hello', 'host', 'mail.example.com');
-        this.plugin.hook_mail(next, this.connection,
-            [new Address('<test@example.com>')]);
+        this.plugin.hook_mail(next, this.connection, [test_addr]);
+    },
+    'txn, relaying, is_private': function (test) {
+        var next = function (rc) {
+            console.log(arguments);
+            test.equal(undefined, rc);
+            test.done();
+        };
+        test.expect(1);
+        this.plugin.cfg.relay.context='myself';
+        this.plugin.cfg.deny_relay.mfrom_fail = true;
+        this.connection.set('remote', 'ip', '127.0.1.1');
+        this.connection.set('remote', 'is_private', true);
+        this.connection.relaying = true;
+        this.connection.set('hello', 'host', 'www.tnpi.net');
+        this.plugin.nu.public_ip = '66.128.51.165';
+        this.plugin.hook_mail(next, this.connection, [new Address('<nonexist@tnpi.net>')]);
     },
 };
 