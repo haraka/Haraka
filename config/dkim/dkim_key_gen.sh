154055478598517d3f5d6f8c6d8acbbe683f74f8
Matt Simerson <matt@tnpi.net>
2017-01-23 12:03:22 -0800

    add a BIND compatible folded key syntax

--- config/dkim/dkim_key_gen.sh
+++ config/dkim/dkim_key_gen.sh
@@ -6,21 +6,21 @@ usage() {
     exit
 }
 
-if [ -z $1 ];
+if [ -z "$1" ];
 then
     usage
 fi
 
 DOMAIN=$1
 SMTPD=$2
-if [ -z $SMTPD ];
+if [ -z "$SMTPD" ];
 then
     SMTPD="www"
 fi
 
 # create a directory for each DKIM signing domain
-mkdir -p $DOMAIN
-cd $DOMAIN
+mkdir -p "$DOMAIN"
+cd "$DOMAIN" || exit
 
 # The selector can be any value that is a valid DNS label
 # create in the common format: mmmYYYY (apr2014)
@@ -35,13 +35,26 @@ openssl genrsa -out private 2048
 chmod 400 private
 openssl rsa -in private -out public -pubout
 
+DNS_NAME="$(tr -d "\n" < selector)._domainkey"
+DNS_ADDRESS="v=DKIM1;p=$(grep -v -e '^-' public | tr -d "\n")"
+
+# fold width is arbitrary, any value between 80 and 255 is reasonable.
+BIND_SPLIT_ADDRESS="$(echo "$DNS_ADDRESS" | fold -w 110 | sed -e 's/^/	"/g; s/$/"/g')"
+
 # make it really easy to publish the public key in DNS
 # by creating a file named 'dns', with instructions
 cat > dns <<EO_DKIM_DNS
 
 Add this TXT record to the $DOMAIN DNS zone.
 
-`cat selector | tr -d "\n"`._domainkey TXT "v=DKIM1;p=`grep -v -e '^-' public | tr -d "\n"`"
+$DNS_NAME    IN   TXT   $DNS_ADDRESS
+
+
+BIND zone file formatted:
+
+$DNS_NAME    IN   TXT (
+$BIND_SPLIT_ADDRESS
+        )
 
 Tell the world that the ONLY mail servers that send mail from this domain are DKIM signed and/or bear our MX and A records.
 
@@ -54,10 +67,6 @@ With DMARC:
 
 _dmarc  TXT "v=DMARC1; p=reject; adkim=s; aspf=r; rua=mailto:dmarc-feedback@$DOMAIN; ruf=mailto:dmarc-feedback@$DOMAIN; pct=100"
 
-With DomainKeys (deprecated)
-
-_domainkey TXT "o=-; t=y; r=postmaster@$DOMAIN"
-
 For more information about DKIM and SPF policy, the documentation within each plugin contains a longer discussion and links to more detailed information:
 
    haraka -h dkim_sign
@@ -67,4 +76,3 @@ For more information about DKIM and SPF policy, the documentation within each pl
 EO_DKIM_DNS
 
 cd ..
-#chown -R $SMTPD:$SMTPD $DOMAIN
