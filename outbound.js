ffbb8be77f4730d1c08fe81225fc4470c356e282
Matt Simerson <matt@tnpi.net>
2017-03-05 13:33:02 -0800

    unbracket

--- outbound.js
+++ outbound.js
@@ -846,23 +846,23 @@ exports.split_to_new_recipients = function (hmail, recipients, response, cb) {
 exports.get_tls_options = function (mx) {
 
     var tls_config = exports.net_utils.load_tls_ini();
+
     var tls_options = { servername: mx.exchange };
     var config_options = [
         'key', 'cert', 'ciphers', 'dhparam',
         'requestCert', 'honorCipherOrder', 'rejectUnauthorized'
     ];
 
-
     for (var i = 0; i < config_options.length; i++) {
         var opt = config_options[i];
-        if (tls_config.main[opt] === undefined) { continue; }
+        if (tls_config.main[opt] === undefined) continue;
         tls_options[opt] = tls_config.main[opt];
     }
 
     if (tls_config.outbound) {
         for (var j = 0; j < config_options.length; j++) {
             var opt2 = config_options[j];
-            if (tls_config.outbound[opt2] === undefined) { continue; }
+            if (tls_config.outbound[opt2] === undefined) continue;
             tls_options[opt2] = tls_config.outbound[opt2];
         }
     }
