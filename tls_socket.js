7c122ce4da8d6dfe22125c82268e3c26e61623f9
Matt Simerson <matt@tnpi.net>
2017-03-31 14:55:09 -0700

    tls: remove interim variables (#1871)
    * - remove interim variables
    * remove unused _controlReleased

--- tls_socket.js
+++ tls_socket.js
@@ -146,9 +146,6 @@ function pipe (cleartext, socket) {
     cleartext.socket = socket;
 
     function onerror (e) {
-        if (cleartext._controlReleased) {
-            cleartext.emit('error', e);
-        }
     }
 
     function onclose () {
@@ -235,21 +232,15 @@ function createServer (cb) {
             cryptoSocket.removeAllListeners('data');
 
             if (!options) options = {};
-
             options.isServer = true;
 
             if (!options.secureContext) {
                 options.secureContext = _getSecureContext(options);
             }
 
-            if (options.enableOCSPStapling) {
-                if (ocsp) {
-                    options.server = pseudoServ;
-                    pseudoServ._sharedCreds = options.secureContext;
-                }
-                else {
-                    log.logerror("OCSP Stapling cannot be enabled because the ocsp module is not loaded");
-                }
+            if (options.enableOCSPStapling && ocsp) {
+                options.server = pseudoServ;
+                pseudoServ._sharedCreds = options.secureContext;
             }
 
             var cleartext = new tls.TLSSocket(cryptoSocket, options);
@@ -262,16 +253,13 @@ function createServer (cb) {
 
             cleartext.on('secure', function () {
                 log.logdebug('TLS secured.');
-                var cert = cleartext.getPeerCertificate();
-                if (cleartext.getCipher) {
-                    var cipher = cleartext.getCipher();
-                }
                 socket.emit('secure');
                 if (cb2) cb2(cleartext.authorized,
-                    cleartext.authorizationError, cert, cipher);
+                    cleartext.authorizationError,
+                    cleartext.getPeerCertificate(),
+                    cleartext.getCipher()
+                    );
             });
-
-            // cleartext._controlReleased = true;
 
             socket.cleartext = cleartext;
 
@@ -334,7 +322,6 @@ function connect (port, host, cb) {
             );
         });
 
-        // cleartext._controlReleased = true;
         socket.cleartext = cleartext;
 
         if (socket._timeout) {
