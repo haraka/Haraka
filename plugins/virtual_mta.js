cd043c0b94895f7ebbe66b8687598ab6a578a003
z.acharki <acharki.zakaria@gmail.com>
2017-04-19 12:07:58 +0100

    "virtual_mta" plugin core small changes

--- plugins/virtual_mta.js
+++ plugins/virtual_mta.js
@@ -5,21 +5,21 @@
 var outbound	= require('./outbound');
 var constants   = require('haraka-constants');
 var config      = require('./config');
-var ip          = require("ip").address(); //Main ip of local server
+var ip          = require('ip').address(); //Main ip of local server
 var host 	    = require('os').hostname().replace(/\\/, '\\057').replace(/:/, '\\072'); //Server hostname
-var vmta = null; 
+var vmta        = null;
 var cfg;
 
 exports.register = function () {
     var plugin = this;
 
-    plugin.loginfo('VMTA configs are fully loaded.');
-    cfg = plugin.config.get('vmta.ini', function () {
+    plugin.loginfo("VMTA configs are fully loaded from 'vmta.ini'.");
+    cfg = plugin.config.get("vmta.ini", function () {
         // This closure will be run for each detected update of my_plugin.ini
         // Re-run the outer function again
         plugin.register();
     });
-    plugin.loginfo('cfg=' + JSON.stringify(cfg));
+    plugin.loginfo(cfg);
 };
 
 exports.hook_queue_outbound = function (next, connection) {
@@ -29,10 +29,10 @@ exports.hook_queue_outbound = function (next, connection) {
     plugin.loginfo("");
     plugin.loginfo("----------- virtual_mta plugin LOG START -----------");
 
-    if( transaction.header.headers.hasOwnProperty('x-vmta') )
+    if( transaction.header.headers.hasOwnProperty("x-vmta") )
     {
         //Get 'x-vmta' from the header
-        vmta = transaction.header.headers['x-vmta'][0].replace("\n", "");
+        vmta = transaction.header.headers["x-vmta"][0].replace("\n", "");
 
         //Check if The specified VMTA is defined in the config file
         if( cfg.hasOwnProperty(vmta) ){
@@ -41,9 +41,9 @@ exports.hook_queue_outbound = function (next, connection) {
             connection.transaction.notes.outbound_helo = cfg[vmta].host;
 
             //Remove parameter from the header
-            transaction.remove_header('x-vmta');
+            transaction.remove_header("x-vmta");
 
-            plugin.loginfo("'x-vmta' Found : "+vmta);
+            plugin.loginfo("'x-vmta' Found '"+vmta+"'");
         }else{
             plugin.logerror("The specified Virtual VMTA '"+vmta+"' does not exist.");
             return next(DENY, "The specified Virtual VMTA '"+vmta+"' does not exist.");
