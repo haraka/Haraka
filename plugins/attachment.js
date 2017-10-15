7cb2b39edc17f6e9949f134bcd3b867c537adf85
Stefan Wawrzyniak <stefanwawrzyniak@icloud.com>
2017-10-15 10:51:57 +0100

    Removed extra space from filename

--- plugins/attachment.js
+++ plugins/attachment.js
@@ -295,7 +295,7 @@ exports.start_attachment = function (connection, ctype, filename, body, stream) 
         stream.pause();
 
         digest = md5.digest('hex');
-        connection.loginfo(plugin, `file="${filename} " ctype="${ctype}" md5=${digest} bytes=${bytes}`);
+        connection.loginfo(plugin, `file="${filename}" ctype="${ctype}" md5=${digest} bytes=${bytes}`);
         txn.notes.attachments.push({
             ctype: ((ct && ct[1]) ? ct[1].toLowerCase() : 'unknown/unknown'),
             filename: (filename ? filename : ''),
