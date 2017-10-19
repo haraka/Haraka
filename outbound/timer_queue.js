87d5cf58c11ab82cad1aae051365ba92d1ba110a
Richard Williams <rw251@users.noreply.github.com>
2017-10-19 16:47:38 +0100

    Updated timer_queue to es6 class and added unit tests. (#2226)
    * Updated timer_queue to es6 class and added unit tests.
    * Added es6 class methods to DKIMSignStream class.

--- outbound/timer_queue.js
+++ outbound/timer_queue.js
@@ -2,68 +2,76 @@
 
 const logger = require('../logger');
 
-function TQTimer (fire_time, cb) {
-    this.fire_time = fire_time;
-    this.cb = cb;
+class TQTimer {
+
+    constructor (fire_time, cb) {
+        this.fire_time = fire_time;
+        this.cb = cb;
+    }
+
+    cancel () {
+        this.cb = null;
+    }
+
 }
 
-TQTimer.prototype.cancel = function () {
-    this.cb = null;
-};
+class TimerQueue {
 
-function TimerQueue (interval) {
-    const self = this;
-    interval = interval || 1000;
-    this.queue = [];
-    this.interval_timer = setInterval(function () { self.fire(); }, interval);
+    constructor (interval) {
+        const self = this;
+        interval = interval || 1000;
+        this.queue = [];
+        this.interval_timer = setInterval(function () { self.fire(); }, interval);
+    }
+
+    add (ms, cb) {
+        const fire_time = Date.now() + ms;
+
+        const timer = new TQTimer(fire_time, cb);
+
+        if ((this.queue.length === 0) ||
+            fire_time >= this.queue[this.queue.length - 1].fire_time) {
+            this.queue.push(timer);
+            return timer;
+        }
+
+        for (let i=0; i < this.queue.length; i++) {
+            if (this.queue[i].fire_time > fire_time) {
+                this.queue.splice(i, 0, timer);
+                return timer;
+            }
+        }
+
+        throw "Should never get here";
+    }
+
+    fire () {
+        if (this.queue.length === 0) return;
+
+        const now = Date.now();
+
+        while (this.queue.length && this.queue[0].fire_time <= now) {
+            const to_run = this.queue.shift();
+            if (to_run.cb) to_run.cb();
+        }
+    }
+
+    length () {
+        return this.queue.length;
+    }
+
+    drain () {
+        logger.logdebug("Draining " + this.queue.length + " items from the queue");
+        while (this.queue.length) {
+            const to_run = this.queue.shift();
+            if (to_run.cb) to_run.cb();
+        }
+    }
+
+    shutdown () {
+        clearInterval(this.interval_timer);
+    }
 }
 
 module.exports = TimerQueue;
 
-TimerQueue.prototype.add = function (ms, cb) {
-    const fire_time = Date.now() + ms;
-
-    const timer = new TQTimer(fire_time, cb);
-
-    if ((this.queue.length === 0) ||
-        fire_time >= this.queue[this.queue.length - 1].fire_time) {
-        this.queue.push(timer);
-        return timer;
-    }
-
-    for (let i=0; i < this.queue.length; i++) {
-        if (this.queue[i].fire_time > fire_time) {
-            this.queue.splice(i, 0, timer);
-            return timer;
-        }
-    }
-
-    throw "Should never get here";
-};
-
-TimerQueue.prototype.fire = function () {
-    if (this.queue.length === 0) return;
-
-    const now = Date.now();
-
-    while (this.queue.length && this.queue[0].fire_time <= now) {
-        const to_run = this.queue.shift();
-        if (to_run.cb) to_run.cb();
-    }
-};
-
-TimerQueue.prototype.length = function () {
-    return this.queue.length;
-};
-
-TimerQueue.prototype.drain = function () {
-    logger.logdebug("Draining " + this.queue.length + " items from the queue");
-    while (this.queue.length) {
-        const to_run = this.queue.shift();
-        if (to_run.cb) to_run.cb();
-    }
-};
-
-TimerQueue.prototype.shutdown = function () {
-    clearInterval(this.interval_timer);
-}
