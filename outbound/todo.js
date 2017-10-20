38b885aaefbabe05edcec54ebb56f487d5da01c6
Matt Simerson <matt@tnpi.net>
2017-10-19 17:43:03 -0700

    outbound/todo: update to es6 classes
    updates #2133

--- outbound/todo.js
+++ outbound/todo.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict';
 
-// TODOItem - queue file header data
-function TODOItem (domain, recipients, transaction) {
-    this.queue_time = Date.now();
-    this.domain = domain;
-    this.rcpt_to = recipients;
-    this.mail_from = transaction.mail_from;
-    this.message_stream = transaction.message_stream;
-    this.notes = transaction.notes;
-    this.uuid = transaction.uuid;
-    return this;
+// queue file header data
+class TODOItem {
+    constructor (domain, recipients, transaction) {
+        this.queue_time = Date.now();
+        this.domain = domain;
+        this.rcpt_to = recipients;
+        this.mail_from = transaction.mail_from;
+        this.message_stream = transaction.message_stream;
+        this.notes = transaction.notes;
+        this.uuid = transaction.uuid;
+    }
 }
 
 module.exports = TODOItem;
