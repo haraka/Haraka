7d2a53abb4aced2a86ba085209baccdbf36de133
Matt Simerson <matt@tnpi.net>
2018-01-22 15:10:35 -0800

    replace _ chars in hostnames with code points
    - fixes #2225
    - also perform replacements on passed in host values
    - add outbound/qfile tests

---
+++ tests/outbound/qfile.js
@@ -0,0 +1,67 @@
+
+const qfile = require('../../outbound/qfile');
+
+exports.parts = {
+    'handles 4': (test) => {
+        test.expect(1)
+        const r = qfile.parts('1484878079415_0_12345_8888.mta1.example.com')
+        // console.log(r);
+        delete r.arrival
+        delete r.uid
+        test.deepEqual(r, {
+            next_attempt: 1484878079415,
+            attempts: 0,
+            pid: 12345,
+            counter: 1,
+            host: 'mta1.example.com',
+            age: 0
+        })
+        test.done()
+    },
+    'handles 7': (test) => {
+        test.expect(1)
+        const r = qfile.parts('1516650518128_1516667073032_8_29538_TkPZWz_1_haraka')
+        // console.log(r);
+        delete r.age;
+        test.deepEqual(r, {
+            arrival: 1516650518128,
+            next_attempt: 1516667073032,
+            attempts: 8,
+            pid: 29538,
+            uid: 'TkPZWz',
+            counter: 1,
+            host: 'haraka',
+        })
+        test.done()
+    },
+    'punts on 5': (test) => {
+        test.expect(1)
+        const r = qfile.parts('1516650518128_1516667073032_8_29538_TkPZWz')
+        test.deepEqual(r, null)
+        test.done()
+    },
+}
+
+exports.hostname = {
+    'hostname, defaults to os.hostname()': function (test) {
+        test.expect(1)
+        const r = qfile.hostname();
+        // console.log(r)
+        test.deepEqual(r, require('os').hostname())
+        test.done()
+    },
+    'hostname, replaces \\ char': function (test) {
+        test.expect(1)
+        const r = qfile.hostname('mta1.exam\\ple.com')
+        // console.log(r)
+        test.deepEqual(r, 'mta1.exam\\057ple.com')
+        test.done()
+    },
+    'hostname, replaces _ char': function (test) {
+        test.expect(1)
+        const r = qfile.hostname('mta1.exam_ple.com')
+        // console.log(r)
+        test.deepEqual(r, 'mta1.exam\\137ple.com')
+        test.done()
+    }
+}
