8548a8e4d8bc8d7c7e3851c84c98cd743450abd3
Matt Simerson <matt@tnpi.net>
2018-02-12 07:26:44 -0800

    connection: assure hostname is set (#2338)
    * connection: assure hostname is set
    
    - default to os.hostname() when config/me does not exist
    
    * remove config/me

--- connection.js
+++ connection.js
@@ -59,7 +59,7 @@ class Connection {
         this.local = {           // legacy property locations
             ip: null,            // c.local_ip
             port: null,          // c.local_port
-            host: config.get('me'),
+            host: config.get('me') || os.hostname(),
             info: 'Haraka',
         };
         this.remote = {
