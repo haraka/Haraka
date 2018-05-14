7e5773aa552eceb202de354634e753ce207040d5
Matt Simerson <matt@tnpi.net>
2018-05-14 16:36:06 -0700

    deprecate plugins/max_unrecognized_commands in favor of limit (#2402)
    * deprecate plugins/max_unrecognized_commands in favor of limit
    - fixes haraka/haraka-plugin-limit#16

--- plugins/max_unrecognized_commands.js
+++ plugins/max_unrecognized_commands.js
@@ -1,25 +1,4 @@
-// Don't let the remote end spew us with unrecognized commands
-// Defaults to 10 max unrecognized commands
 
-exports.hook_connect = function (next, connection) {
-    const plugin = this;
-    connection.results.add(plugin, {
-        max: plugin.config.get('max_unrecognized_commands') || 10,
-        count: 0,
-    });
-    return next();
-}
-
-exports.hook_unrecognized_command = function (next, connection, params) {
-    const plugin = this;
-
-    connection.results.add(plugin, {fail: `Unrecognized command: ${params}`, emit: true});
-    connection.results.incr(plugin, {count: 1});
-
-    const uc = connection.results.get('max_unrecognized_commands');
-    if (uc.count >= uc.max) {
-        connection.loginfo(plugin, "Closing connection. Too many bad commands.");
-        return next(DENYDISCONNECT, "Too many bad commands");
-    }
-    return next();
+exports.register = function () {
+    this.lognotice("the plugin max_unrecognized_commands has been replaced by 'limit'. Please update config/plugins");
 }
