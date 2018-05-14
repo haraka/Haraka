c860295624e9a634a6e7452a420888b9e9e56e6f
Matt Simerson <matt@tnpi.net>
2018-05-14 16:29:59 -0700

    ws fix

--- plugins/max_unrecognized_commands.js
+++ plugins/max_unrecognized_commands.js
@@ -1,4 +1,4 @@
 
 exports.register = function () {
-  this.lognotice("the plugin max_unrecognized_commands has been replaced by 'limit'. Please update config/plugins");
+    this.lognotice("the plugin max_unrecognized_commands has been replaced by 'limit'. Please update config/plugins");
 }
