50458be39539dadce03bfeb4a73b7b340863c1c6
Matt Sergeant <helpme+github@gmail.com>
2018-07-15 19:07:39 -0400

    Further fixes including merging #2472 changes

--- outbound/tls.js
+++ outbound/tls.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const logger       = require('../logger');
-const hkredis      = require('haraka-plugin-redis');
 const tls_socket   = require('../tls_socket');
+const hkredis      = require('haraka-plugin-redis');
+const config       = require('haraka-config');
+
+const inheritable_opts = [
+    'key', 'cert', 'ciphers', 'dhparam',
+    'requestCert', 'honorCipherOrder', 'rejectUnauthorized'
+];
 
 class OutboundTLS {
     constructor () {
@@ -14,11 +20,6 @@ class OutboundTLS {
         const tls_cfg = tls_socket.load_tls_ini({role: 'client'});
         const cfg = JSON.parse(JSON.stringify(tls_cfg.outbound || {}));
         cfg.redis = tls_cfg.redis; // Don't clone - contains methods
-
-        const inheritable_opts = [
-            'key', 'cert', 'ciphers', 'dhparam',
-            'requestCert', 'honorCipherOrder', 'rejectUnauthorized'
-        ];
 
         for (const opt of inheritable_opts) {
             if (cfg[opt] === undefined) {
@@ -34,31 +35,30 @@ class OutboundTLS {
             if (Array.isArray(cfg.key)) {
                 cfg.key = cfg.key[0];
             }
-            cfg.key = exports.config.get(cfg.key, 'binary');
+            cfg.key = config.get(cfg.key, 'binary');
         }
 
         if (cfg.dhparam) {
-            cfg.dhparam = exports.config.get(cfg.dhparam, 'binary');
+            cfg.dhparam = config.get(cfg.dhparam, 'binary');
         }
 
         if (cfg.cert) {
             if (Array.isArray(cfg.cert)) {
                 cfg.cert = cfg.cert[0];
             }
-            cfg.cert = exports.config.get(cfg.cert, 'binary');
+            cfg.cert = config.get(cfg.cert, 'binary');
         }
 
         this.cfg = cfg;
     }
 
     init (cb) {
-        if (this.cfg.redis.disable_for_failed_hosts) { // which means changing this var in-flight won't work
-            logger.logdebug(this, 'Will disable outbound TLS for failing TLS hosts');
-            Object.assign(this, hkredis);
-            this.merge_redis_ini();
-            return this.init_redis_plugin(cb);
-        }
-        return cb();
+        // changing this var in-flight won't work
+        if (this.cfg.redis && !this.cfg.redis.disable_for_failed_hosts) return cb();
+        logger.logdebug(this, 'Will disable outbound TLS for failing TLS hosts');
+        Object.assign(this, hkredis);
+        this.merge_redis_ini();
+        this.init_redis_plugin(cb);
     }
 
     get_tls_options (mx) {
