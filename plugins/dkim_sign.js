7fa2f7d331a1917b3ee44d7ef50713b6dff30264
analogic <info@analogic.cz>
2018-12-09 17:20:51 +0100

    Update dkim_sign.js

--- plugins/dkim_sign.js
+++ plugins/dkim_sign.js
@@ -89,6 +89,12 @@ class DKIMSignStream extends Stream {
             this.hash.update(le);
             this.buffer = { ar: [], len: 0 };
         }
+        
+        // If body is empty it still requires to contain CRLF
+        // RFC6376 - 3.4.3. The "simple" Body Canonicalization Algorithm
+        if (this.buffer.len == 0) {
+            this.hash.update(new Buffer("\r\n"));
+        }
 
         const bodyhash = this.hash.digest('base64');
 
