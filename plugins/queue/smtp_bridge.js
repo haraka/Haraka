20e2650d63d10f9637f01442a4ce2a8704ab014c
kale-io <cos+a@iuvo.ai>
2019-03-26 21:12:56 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- plugins/queue/smtp_bridge.js
+++ plugins/queue/smtp_bridge.js
@@ -7,12 +7,12 @@ exports.register = function () {
 
 exports.load_flat_ini = function () {
     const plugin = this;
-    plugin.cfg = plugin.config.get('smtp_bridge.ini', function () {
+    plugin.cfg = plugin.config.get('smtp_bridge.ini', () => {
         plugin.load_flat_ini();
     });
 }
 
-exports.hook_data_post = function (next, connection) {
+exports.hook_data_post = (next, connection) => {
     const txn = connection.transaction;
     // Copy auth notes to transaction notes so they're available in hmail.todo.notes
     txn.notes.auth_user = connection.notes.auth_user;
