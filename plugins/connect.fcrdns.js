7e5713f6b8f6f3446ea1ddbc8d316d5fe196261a
Matt Simerson <matt@tnpi.net>
2019-08-04 21:41:16 -0700

    replace deprecated plugin stubs with list

--- plugins/connect.fcrdns.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-exports.register = function () {
-    this.logerror('deprecated! Moved to https://github.com/haraka/haraka-plugin-fcrdns');
-}
-
--- plugins/rate_limit.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-exports.register = function () {
-    this.logerror('deprecated! See https://github.com/haraka/haraka-plugin-limit');
-}
