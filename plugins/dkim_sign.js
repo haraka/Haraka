0e48fbd92815958a875865c65ed109079c461fac
noogen <tom@noogen.net>
2019-09-09 23:50:48 -0500

    fix issue #2668 renaming reserved kw/property of 'domain' to 'domain_name'

--- plugins/dkim_sign.js
+++ plugins/dkim_sign.js
@@ -15,7 +15,9 @@ class DKIMSignStream extends Stream {
         super();
 
         this.selector = props.selector;
-        this.domain = props.domain;
+
+        // fix issue #2668 renaming reserved kw/property of 'domain' to 'domain_name'
+        this.domain_name = props.domain_name || props.domain;
         this.private_key = props.private_key;
         this.headers_to_sign = props.headers;
         this.header = header;
@@ -117,7 +119,7 @@ class DKIMSignStream extends Stream {
 
         // Create DKIM header
         let dkim_header = 'v=1;a=rsa-sha256;bh=' + bodyhash +
-                        ';c=relaxed/simple;d=' + this.domain +
+                        ';c=relaxed/simple;d=' + this.domain_name +
                         ';h=' + headers.join(':') +
                         ';s=' + this.selector +
                         ';b=';
@@ -252,9 +254,9 @@ exports.get_sign_properties = function (connection, done) {
 
             connection.transaction.results.add(plugin, {msg: 'using default key', emit: true });
 
-            props.domain = plugin.cfg.main.domain;
+            props.domain      = plugin.cfg.main.domain;
             props.private_key = plugin.private_key;
-            props.selector = plugin.cfg.main.selector;
+            props.selector    = plugin.cfg.main.selector;
 
             return done(null, props)
         }
