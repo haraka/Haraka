9f2d69e108b174efd4bf5a9fbeafe9eb111a4c93
superman20 <superman20@users.noreply.github.com>
2021-10-20 13:05:24 -0400

    Update DKIM test to accommodate new formatting

--- tests/plugins/dkim_signer.js
+++ tests/plugins/dkim_signer.js
@@ -33,7 +33,7 @@ frcCV1k9oG9oKj3dpUqdJg1PxRT2RSN/XKdLCPjaYaY=
 function getValueFromDKIM (dkim_header, key) {
     const kv = dkim_header.split(';');
     for (let i = 0, len = kv.length; i < len; i++) {
-        const arr = kv[i].match(/^([^=]+)=(.*)$/);
+        const arr = kv[i].match(/^\s*([^=]+)=(.*)$/);
         if (arr[1] === key) {
             return arr[2];
         }
