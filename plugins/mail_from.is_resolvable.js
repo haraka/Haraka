09a914e083904657669c57ab87d23c453727a6de
lnedry <lnedry@users.noreply.github.com>
2025-01-29 18:50:49 -0500

    Update mail_from.is_resolvable.js
    Refactored to better handle promises and timeouts

--- plugins/mail_from.is_resolvable.js
+++ plugins/mail_from.is_resolvable.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
 // Check MAIL FROM domain is resolvable to an MX
-const net = require('node:net');
-const net_utils = require('haraka-net-utils');
+const net = require('node:net')
+const net_utils = require('haraka-net-utils')
 
 exports.register = function () {
-    this.load_ini();
+    this.load_ini()
 }
 
 exports.load_ini = function () {
@@ -15,8 +15,8 @@ exports.load_ini = function () {
             '+reject.no_mx',
         ],
     }, () => {
-        this.load_ini();
-    });
+        this.load_ini()
+    })
 
     // compat. Sunset 4.0
     if (this.cfg.main.reject_no_mx) {
@@ -24,82 +24,80 @@ exports.load_ini = function () {
     }
 
     this.re_bogus_ip = new RegExp(this.cfg.main.re_bogus_ip ||
-            '^(?:0\\.0\\.0\\.0|255\\.255\\.255\\.255|127\\.)' );
+            '^(?:0\\.0\\.0\\.0|255\\.255\\.255\\.255|127\\.)' )
 }
 
 exports.hook_mail = async function (next, connection, params) {
-    const plugin = this;
-    const mail_from = params[0];
-    const txn = connection?.transaction;
-    if (!txn) return next();
-    const { results } = txn;
+    const mail_from = params[0]
+    const {results} = connection.transaction
 
     // ignore MAIL FROM without an @
     if (!mail_from.host) {
-        results.add(plugin, {skip: 'null host'});
-        return next();
+        results.add(this, {skip: 'null host'})
+        return next()
     }
 
-    const domain = mail_from.host;
+    const domain = mail_from.host
 
-    function mxErr (err) {
-        results.add(plugin, {err: `${domain}:${err.message}`});
-        return next(DENYSOFT, `Temp. resolver error (${err.code})`);
+    connection.logdebug(this, `resolving MX for domain ${domain}`)
+
+    let exchanges
+    try {
+        exchanges = await net_utils.get_mx(domain)
+    }
+    catch (err) {
+        connection.logwarn(this, `Temporary resolver error: ${err.code}`)
+        results.add(this, {err: `${domain}:${err.message}`})
+        return next(DENYSOFT, `Temp. resolver error (${err.code})`)
     }
 
-    connection.loginfo(plugin, `resolving MX for domain ${domain}`)
+    connection.logdebug(this, `${domain}: MX => ${JSON.stringify(exchanges)}`)
 
-    net_utils
-        .get_mx(domain)
-        .then((exchanges) => {
-            if (!txn) return;
+    if (!exchanges || !exchanges.length) {
+        results.add(this, {fail: 'has_fwd_dns', emit: true})
+        return next(
+            ((this.cfg.reject.no_mx) ? DENY : DENYSOFT),
+            'No MX for your FROM address'
+        )
+    }
 
-            connection.loginfo(plugin, `${domain}: MX => ${JSON.stringify(exchanges)}`)
-
-            if (!exchanges || !exchanges.length) {
-                results.add(this, {fail: 'has_fwd_dns'});
-                return next(
-                    ((this.cfg.reject.no_mx) ? DENY : DENYSOFT),
-                    'No MX for your FROM address'
-                );
+    if (this.cfg.main.allow_mx_ip) {
+        for (const mx of exchanges) {
+            if ((net.isIPv4(mx.exchange) && !this.re_bogus_ip.test(mx.exchange)) ||
+                (net.isIPv6(mx.exchange) && !net_utils.ipv6_bogus(mx.exchange))) {
+                results.add(this, {pass: 'implicit_mx', emit: true})
+                return next()
             }
+        }
+    }
 
-            if (this.cfg.main.allow_mx_ip) {
-                for (const mx of exchanges) {
-                    if (net.isIPv4(mx.exchange) && !this.re_bogus_ip.test(mx.exchange)) {
-                        txn.results.add(this, {pass: 'implicit_mx', emit: true});
-                        return next()
-                    }
-                    if (net.isIPv6(mx.exchange) && !net_utils.ipv6_bogus(mx.exchange)) {
-                        txn.results.add(this, {pass: 'implicit_mx', emit: true});
+    // filter out the implicit MX and resolve the remaining MX hostnames
+    const mx_hostnames = exchanges.filter(a => (a.exchange && !net.isIP(a.exchange)))
+    if (mx_hostnames.length) {
+        try {
+            const resolved = await net_utils.resolve_mx_hosts(mx_hostnames)
+            connection.logdebug(this, `resolved MX => ${JSON.stringify(resolved)}`)
+            if (resolved.length) {
+                for (const mx of resolved) {
+                    if ((net.isIPv4(mx.exchange) && !this.re_bogus_ip.test(mx.exchange)) ||
+                        (net.isIPv6(mx.exchange) && !net_utils.ipv6_bogus(mx.exchange))) {
+                        results.add(this, {pass: 'has_fwd_dns', emit: true})
                         return next()
                     }
                 }
             }
+        }
+        catch (err) {
+            // resolve_mx_hosts ignores errors so this is unlikely to happen
+            connection.logwarn(this, `Temp. resolver error: ${err.code}`)
+            results.add(this, {err: `${domain}:${err.message}`})
+            return next(DENYSOFT, `Temp. resolver error (${err.code})`)
+        }
+    }
 
-            // filter out the implicit MX and resolve the MX hostnames
-            net_utils
-                .resolve_mx_hosts(exchanges.filter(a => !net.isIP(a.exchange)))
-                .then(resolved => {
-                    connection.loginfo(plugin, `resolved MX => ${JSON.stringify(resolved)}`);
-
-                    for (const mx of resolved) {
-                        if (net.isIPv4(mx.exchange) && !this.re_bogus_ip.test(mx.exchange)) {
-                            txn.results.add(this, {pass: 'has_fwd_dns', emit: true});
-                            return next()
-                        }
-                        if (net.isIPv6(mx.exchange) && !net_utils.ipv6_bogus(mx.exchange)) {
-                            txn.results.add(this, {pass: 'has_fwd_dns', emit: true});
-                            return next()
-                        }
-                    }
-
-                    return next(
-                        ((this.cfg.reject.no_mx) ? DENY : DENYSOFT),
-                        'No valid MX for your FROM address'
-                    );
-                })
-                .catch(mxErr)
-        })
-        .catch(mxErr)
+    results.add(this, {fail: 'has_fwd_dns', emit: true})
+    return next(
+        ((this.cfg.reject.no_mx) ? DENY : DENYSOFT),
+        'No valid MX for your FROM address'
+    )
 }
